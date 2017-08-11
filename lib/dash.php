d3dbfcc1a0c39b0944a593ee5f4b159f26b82c78
Reza <rm.biqarar@gmail.com>
2017-08-11 15:42:17 +0430

    add timezone library

--- lib/dash.php
+++ lib/dash.php
@@ -6,7 +6,7 @@ namespace lib;
 class dash
 {
 	// @var dash core current version
-	const version = '10.0.5';
+	const version = '10.0.6';
 
 	// @var dash core current commit number
 	// now get it automatically from git commands
