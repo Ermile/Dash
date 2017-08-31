f3d014b3e4288344c47f2fe3d70beb7a57ed99e0
Reza <rm.biqarar@gmail.com>
2017-08-31 12:10:50 +0430

    add index of termusage table

--- addons/includes/cls/database/upgrade/(db_name)/v.1.3.8_drop_triggers.sql
+++ addons/includes/cls/database/upgrade/(db_name)/v.1.3.8_drop_triggers.sql
@@ -4,3 +4,8 @@ DROP TRIGGER `termusages_change_terms_count_usercount_on_insert`;
 ALTER TABLE `terms` CHANGE `usercount` `usercount` INT(10) UNSIGNED NULL DEFAULT NULL;
 ALTER TABLE `termusages` DROP FOREIGN KEY  `termusages_terms_id`;
 ALTER TABLE `termusages` DROP INDEX `term+type+object_unique`;
+
+ALTER TABLE `termusages` ADD INDEX(`term_id`);
+ALTER TABLE `termusages` ADD INDEX(`related_id`);
+ALTER TABLE `termusages` ADD INDEX(`related`);
+ALTER TABLE `termusages` ADD INDEX(`status`);
