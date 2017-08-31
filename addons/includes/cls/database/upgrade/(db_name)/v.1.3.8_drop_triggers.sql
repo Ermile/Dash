c843f14733a76637aa6dcebe35245d0b627dade7
Reza <rm.biqarar@gmail.com>
2017-08-31 11:25:02 +0430

    drop index of termusages table

--- addons/includes/cls/database/upgrade/(db_name)/v.1.3.8_drop_triggers.sql
+++ addons/includes/cls/database/upgrade/(db_name)/v.1.3.8_drop_triggers.sql
@@ -2,3 +2,5 @@ DROP TRIGGER `termusages_change_terms_count_usercount_on_delete`;
 DROP TRIGGER `termusages_change_terms_count_usercount_on_update`;
 DROP TRIGGER `termusages_change_terms_count_usercount_on_insert`;
 ALTER TABLE `terms` CHANGE `usercount` `usercount` INT(10) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `termusages` DROP FOREIGN KEY  `termusages_terms_id`;
+ALTER TABLE `termusages` DROP INDEX `term+type+object_unique`;
