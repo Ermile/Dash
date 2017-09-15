36c6b7622463afde0ee358513dfe8196d55d9fc2
Reza <rm.biqarar@gmail.com>
2017-09-15 13:07:31 +0430

    remove add users in install database

--- addons/includes/cls/database/install/(db_name)/v.0.0.1_users.sql
+++ addons/includes/cls/database/install/(db_name)/v.0.0.1_users.sql
@@ -52,11 +52,3 @@ CREATE TABLE `users` (
 ALTER TABLE `users` ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `users` MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
-
-INSERT INTO `users` (`id`, `mobile`, `email`, `password`, `displayname`, `meta`, `status`, `permission`, `datecreated`, `datemodified`) VALUES
-(1, '989357269759', 'J.Evazzadeh@gmail.com', '$2y$07$9wj8/jDeQKyY0t0IcUf.xOEy98uf6BaSS7Tg28swrKUDxdKzUVfsy', 'Javad Evazzadeh', NULL, 'active', 'admin', '2015-01-01 00:00:00', NULL),
-(2, '989356032043', 'itb.baravak@gmail.com', '$2y$07$ZRUphEsEn9bK8inKBfYt.efVoZDgBaoNfZz0uVRqRGvH9.che.Bqq', 'Hasan Salehi', NULL, 'active', NULL, '2015-01-02 00:00:00', NULL),
-(3, '989190499033', 'ahmadkarimi1991@gmail.com', '$2y$07$bLbhODUiPBFfbTU8V./m5OAYdkH2DP7uCQI2fVLubq7X/LdFQTeH.', 'Ahmad Karimi', NULL, 'active', NULL, '2015-01-03 00:00:00', NULL),
-(4, '989109610612', 'rm.biqarar@gmail.com', '$2y$07$k.Vi7QCpdym637.6rwbm2.u1tdMi4jyWFUg7YgNv.XnBFOP1.7W/y', 'Reza Mohiti', NULL, 'active', NULL, '2015-01-04 00:00:00', NULL);
-
