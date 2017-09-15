9f717d68796824511a54d8e5b1fde19f452c6905
Reza <rm.biqarar@gmail.com>
2017-09-15 23:57:03 +0430

    add cat in permlist

--- addons/content_cp/permission/view.php
+++ addons/content_cp/permission/view.php
@@ -6,6 +6,15 @@ class view extends \addons\content_cp\main\view
 	public function view_add($_args)
 	{
 		$list_perm = \lib\permission::list();
+
+		if(is_array($list_perm))
+		{
+			$cat = array_column($list_perm, 'cat');
+			$cat = array_unique($cat);
+			$cat = array_filter($cat);
+			$this->data->perm_cat = $cat;
+		}
+
 		$this->data->permission_list = $list_perm;
 		$id = isset($_args->match->url[0][1]) ? $_args->match->url[0][1] : null;
 		$id = \lib\utility\shortURL::decode($id);
