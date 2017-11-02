d565e08c40e46c7114756bfef8b5fb6817a3936a
Reza <rm.biqarar@gmail.com>
2017-11-02 17:08:16 +0330

    add app for upload file

---
+++ lib/app/file.php
@@ -0,0 +1,115 @@
+<?php
+namespace lib\app;
+use \lib\utility;
+use \lib\debug;
+use \lib\utility\upload;
+
+class file
+{
+
+	public static function upload($_options = [])
+	{
+		\lib\app::variable($_options);
+
+		$default_options =
+		[
+			'upload_name' => \lib\app::request('upload_name'),
+			'url'         => null,
+			'debug'       => true,
+		];
+
+		if(!is_array($_options))
+		{
+			$_options = [];
+		}
+
+		$_options = array_merge($default_options, $_options);
+
+		if(\lib\app::request('url') && !$_options['url'])
+		{
+			$_options['url'] = \lib\app::request('url');
+		}
+
+		$file_path = false;
+
+		if($_options['url'])
+		{
+			$file_path = true;
+		}
+		elseif(!utility::files($_options['upload_name']))
+		{
+			\lib\debug::error(T_("Unable to upload, because of selected upload name"), 'upload_name', 'arguments');
+			return false;
+		}
+
+		$ready_upload            = [];
+		$ready_upload['user_id'] = \lib\user::id();
+		$ready_upload['debug']   = $_options['debug'];
+
+		if($file_path)
+		{
+			$ready_upload['file_path'] = $_options['url'];
+		}
+		else
+		{
+			$ready_upload['upload_name'] = $_options['upload_name'];
+		}
+
+		// if(\lib\permission::access('admin:admin:view', null, \lib\user::id()))
+		// {
+		// 	$ready_upload['status'] = 'publish';
+		// }
+		// else
+		// {
+		// 	$ready_upload['status'] = 'draft';
+		// }
+
+		$ready_upload['status'] = 'publish';
+
+		// $ready_upload['user_size_remaining'] = self::remaining(\lib\user::id());
+
+		upload::$extentions = ['png', 'jpeg', 'jpg'];
+
+		$upload      = upload::upload($ready_upload);
+
+		if(!debug::$status)
+		{
+			return false;
+		}
+
+		$file_detail = \lib\temp::get('upload');
+		$file_id     = null;
+
+		// if(isset($file_detail['size']))
+		// {
+		// 	self::user_size_plus(\lib\user::id(), $file_detail['size']);
+		// }
+
+		if(isset($file_detail['id']) && is_numeric($file_detail['id']))
+		{
+			$file_id = $file_detail['id'];
+		}
+		else
+		{
+			return debug::error(T_("Can not upload file. undefined error"));
+		}
+
+		$file_id_code = null;
+
+		if($file_id)
+		{
+			$file_id_code = utility\shortURL::encode($file_id);
+		}
+
+		$url = null;
+
+		if(isset($file_detail['url']))
+		{
+			$url = Protocol."://" . \lib\router::get_root_domain() . '/'. $file_detail['url'];
+		}
+
+		return ['code' => $file_id_code, 'url' => $url];
+	}
+}
+
+?>
