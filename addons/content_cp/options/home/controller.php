efa10ca4b387ce982134fa02d71e8c3e8965de0f
Reza <rm.biqarar@gmail.com>
2017-12-30 11:29:44 +0330

    add home options in content_cp

---
+++ addons/content_cp/options/home/controller.php
@@ -0,0 +1,8 @@
+<?php
+namespace addons\content_cp\options\home;
+
+class controller extends \addons\content_cp\main\controller
+{
+
+}
+?>
---
+++ addons/content_cp/options/home/view.php
@@ -0,0 +1,8 @@
+<?php
+namespace addons\content_cp\options\home;
+
+class view extends \addons\content_cp\main\view
+{
+
+}
+?>
