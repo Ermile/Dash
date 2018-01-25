fb367e8d146f0c78957970b08a54b856f2de323d
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-01-25 13:43:46 +0330

    change edit view

--- addons/content_cp/posts/edit/view.php
+++ addons/content_cp/posts/edit/view.php
@@ -17,6 +17,27 @@ class view extends \addons\content_cp\posts\main\view
 
 		$this->data->dataRaw = $detail;
 		$this->data->cat_list = \lib\app\term::cat_list();
+
+
+
+		$this->data->page['title'] = T_("Edit post");
+		$this->data->page['desc']  = T_("You can change everything, change url and add gallery or some other change");
+
+		$this->data->page['badge']['link'] = $this->data->modulePath. $this->data->moduleType;
+		$this->data->page['badge']['text'] = T_('Back to list of posts');
+
+		$myType = \lib\utility::get('type');
+		if($myType)
+		{
+			switch ($myType)
+			{
+				case 'page':
+					$this->data->page['title'] = T_('Edit page');
+					$this->data->page['badge']['text'] = T_('Back to list of pages');
+					break;
+			}
+		}
+
 	}
 }
 ?>
