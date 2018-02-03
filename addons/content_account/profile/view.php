65cb2fc4096095a983d5d256a4df0b1f4a0f70ba
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-02-03 11:58:57 +0330

    fix edit profile page

--- addons/content_account/profile/view.php
+++ addons/content_account/profile/view.php
@@ -3,6 +3,12 @@ namespace content_account\profile;
 
 class view extends \content_account\main\view
 {
+	public function config()
+	{
+		$this->data->page['title'] = T_("Edit profile");
+		$this->data->page['desc']  = T_("Check your profile and edit name or avatar of your account");
+	}
+
 	public function view_profile()
 	{
 		if($this->login('unit_id'))
