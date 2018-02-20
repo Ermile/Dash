0bea2ef4b967f041e81597c35ce9702547e64c62
Reza <rm.biqarar@gmail.com>
2018-02-20 22:57:57 +0330

    complete new pagination syntax

--- lib/db/mysql/tools/pagination.php
+++ lib/db/mysql/tools/pagination.php
@@ -6,45 +6,14 @@ trait pagination
 	/**
 	 * get pagnation
 	 *
-	 * @param      <type>  $_query   The query
+	 * @param      <type>  $_total_rows   The query
 	 * @param      <type>  $_length  The length
 	 *
 	 * @return     <type>  array [startlimit, endlimit]
 	 */
-	public static function pagnation($_query, $_length, $_force = true)
+	public static function pagnation($_total_rows, $_length)
 	{
-		// return \lib\utility\pagination::init($_query, $_length);
-
-		if($_force)
-		{
-			if(is_int($_query))
-			{
-				$count = $_query;
-			}
-			else
-			{
-				$count = self::query($_query);
-				if($count && is_a($count, "mysqli_result"))
-				{
-					$count = mysqli_num_rows($count);
-				}
-				else
-				{
-					$count = 0;
-				}
-			}
-			\lib\main::$controller->pagnation_make($count, $_length);
-			$current = \lib\main::$controller->pagnation_get('current');
-
-			$length = \lib\main::$controller->pagnation_get('length');
-			$limit_start = ($current - 1) * $length ;
-			if($limit_start < 0)
-			{
-				$limit_start = 0;
-			}
-			$limit_end = $length;
-			return [$limit_start, $limit_end];
-		}
+		return \lib\utility\pagination::init($_total_rows, $_length);
 	}
 }
 ?>
