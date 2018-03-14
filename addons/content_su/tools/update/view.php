6d774c1920c5646bd3c2464523ca94772e765073
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-03-15 01:52:53 +0330

    complete design of update page

--- addons/content_su/tools/update/view.php
+++ addons/content_su/tools/update/view.php
@@ -8,6 +8,18 @@ class view extends \addons\content_su\main\view
 		parent::config();
 		$this->data->page['title'] = T_("Update");
 		$this->data->page['desc']  = T_('Check curent version of dash and update to latest version if available.');
+
+		$this->data->dashLoc = null;;
+
+		// go to root url
+		if(is_dir(root. '/dash'))
+		{
+			$this->data->dashLoc = T_('Inside project'). ' <span class="sf-chain-broken fc-green"></span>';
+		}
+		elseif(is_dir(root. '../dash'))
+		{
+			$this->data->dashLoc = T_('Global'). ' <span class="sf-globe-1 fc-red"></span>';
+		}
 	}
 }
 ?>
