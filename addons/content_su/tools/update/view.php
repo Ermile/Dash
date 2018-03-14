75ac4328e89bac1cfee7c0b75e3e668cfc309971
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-03-15 01:23:41 +0330

    add simple display for update process

--- addons/content_su/tools/update/view.php
+++ addons/content_su/tools/update/view.php
@@ -3,6 +3,11 @@ namespace addons\content_su\tools\update;
 
 class view extends \addons\content_su\main\view
 {
-
+	public function config()
+	{
+		parent::config();
+		$this->data->page['title'] = T_("Update");
+		$this->data->page['desc']  = T_('Check curent version of dash and update to latest version if available.');
+	}
 }
 ?>
