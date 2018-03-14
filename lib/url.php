58a416deb13af4a193fbe821c794647f04bed5f8
Reza <rm.biqarar@gmail.com>
2018-03-14 13:55:49 +0330

    merge controller rul whit lib\url

--- lib/url.php
+++ lib/url.php
@@ -13,6 +13,8 @@ class url
 	private static $split_host = [];
 	private static $path_split = [];
 	private static $host_is_ip = false;
+	// use in old controller url syntax TO DO: remove it
+	private static $base       = null;
 
 
 	/**
@@ -61,6 +63,298 @@ class url
 		self::$url['full']      = self::_full();
 		self::$url['full2']     = self::_full2();
 
+
+		//--------------------------------------------------------------- CONTROLLER INDEX OF URL
+		// --- controller url syntax
+		$base = null;
+        $base .= \lib\url::protocol(). '://';
+        $base .= \lib\url::host();
+        if($lang = \lib\url::lang())
+    	{
+    		$base .= '/'. $lang;
+    	}
+    	self::$base = $base;
+
+		self::$url['base']         = self::_OLD_base(); // --------- duplicate index of array
+		self::$url['baseContent']  = self::_baseContent();
+		self::$url['baseFull']     = self::_baseFull();
+		self::$url['baseRaw']      = self::_baseRaw();
+		self::$url['prefix']       = self::_prefix();
+		self::$url['sub']          = self::_subdomain(); // use new syntax; ! changed the name
+		self::$url['path']         = self::_OLD_path(); // --------- duplicate index of array
+		self::$url['breadcrumb']   = self::_breadcrumb();
+		self::$url['param']        = self::_query(); // use new syntax; ! changed the name
+		self::$url['domain']       = self::_root(); // use new syntax; ! changed the name | // --------- duplicate index of array
+		self::$url['raw']          = self::_domain(); // use new syntax; ! changed the name
+		self::$url['root']         = self::_base(); // use new syntax; ! changed the name
+		self::$url['MainProtocol'] = self::_protocol(); // use new syntax; ! changed the name // --------- duplicate index of array
+		self::$url['MainSite']     = self::_domain(); // use new syntax; ! changed the name
+		self::$url['module']       = self::_module();
+		self::$url['child']        = self::_child();
+		self::$url['tags']         = self::_tags(); // is null !!!
+		self::$url['cats']         = self::_cats(); // is null !!!
+		self::$url['pages']        = self::_pages(); // is null !!!
+		self::$url['LoginService'] = self::_LoginService();
+		self::$url['account']      = self::_account();
+		self::$url['MainService']  = self::_MainService();
+
+		// example : http://saeed.jibres.local/a/thirdparty/edit?id=5&page=8
+		// 'query'        => 'id=5&page=8'
+		// 'protocol'     => 'http'
+		// 'port'         => 80
+		// 'tld'          => 'local'
+		// 'subdomain'    => 'saeed'
+		// 'root'         => 'http://saeed.jibres.local'
+		// 'domain'       => 'jibres'
+		// 'host'         => 'saeed.jibres.local'
+		// 'base'         => 'http://saeed.jibres.local'
+		// 'path'         => 'thirdparty/edit'
+		// 'lang'         => null
+		// 'dir'          =>
+		// [
+		// 	0 => 'a'
+		// 	1 => 'thirdparty'
+		// 	2 => 'edit'
+		// ]
+		// 'content'      => 'a'
+		// 'property'     =>  []
+
+
+		// 'full'         => 'http://saeed.jibres.local/a/thirdparty/edit?id=5&page=8'
+		// 'full2'        => 'http://saeed.jibres.local/a/thirdparty/edit'
+		// 'baseContent'  => 'http://saeed.jibres.local/a'
+		// 'baseFull'     => 'http://saeed.jibres.local/a'
+		// 'baseRaw'      => 'http://saeed.jibres.local/a'
+		// 'prefix'       => '/a'
+		// 'sub'          => 'saeed'
+		// 'breadcrumb'   =>
+		// [
+		// 	1 => 'thirdparty'
+		// 	2 => 'edit'
+		// ]
+		// 'param'        => 'id=5&page=8'
+		// 'raw'          => 'jibres.local'
+		// 'MainProtocol' => 'http'
+		// 'MainSite'     => 'http://saeed.jibres.local.local'
+		// 'module'       => 'thirdparty'
+		// 'child'        => null
+		// 'tags'         => null
+		// 'cats'         => null
+		// 'pages'        => null
+		// 'LoginService' => 'http://jibres/account'
+		// 'account'      => 'http://jibres/account'
+		// 'MainService'  => 'http://ermile.local'
+	}
+
+	private static function _OLD_base()
+	{
+		return self::$base;
+	}
+
+	private static function _baseContent()
+	{
+		$base = null;
+        $base .= \lib\url::protocol(). '://';
+        $base .= \lib\url::host();
+		$new_url = $base;
+		if($content = \lib\url::content())
+		{
+			$new_url .= '/'. $content;
+		}
+		return $new_url;
+	}
+
+	private static function _baseFull()
+	{
+		$new_url = self::$base;
+		if($content = \lib\url::content())
+		{
+			$new_url .= '/'. $content;
+		}
+		return $new_url;
+	}
+
+	private static function _baseRaw()
+	{
+		$new_url = self::$base;
+		if($content = \lib\url::content())
+		{
+			$new_url .= '/'. $content;
+		}
+		return $new_url;
+	}
+
+	private static function _prefix()
+	{
+		$prefix = null;
+		if($lang = \lib\url::lang())
+		{
+			$prefix .= $lang;
+		}
+		if($content = \lib\url::content())
+		{
+			$prefix .= '/'. $content;
+		}
+		$new_url = $prefix;
+		return $new_url;
+	}
+
+
+	private static function _OLD_path()
+	{
+		$path = \lib\url::dir();
+		if(isset($path[0]) && $path[0] === \lib\url::content())
+		{
+			unset($path[0]);
+		}
+		if(is_array($path) && $path)
+		{
+			$path = implode('/', $path);
+		}
+		else
+		{
+			$path = null;
+		}
+
+		if($lang = \lib\url::lang())
+		{
+			$path = $lang . '/'. $path;
+		}
+
+		$new_url = $path;
+		return $new_url;
+	}
+
+	private static function _breadcrumb()
+	{
+		$breadcrumb = \lib\url::dir();
+		if(isset($breadcrumb[0]) && $breadcrumb[0] === \lib\url::content())
+		{
+			unset($breadcrumb[0]);
+		}
+
+		if(is_array($breadcrumb) && $breadcrumb)
+		{
+			$temp = [];
+			foreach ($breadcrumb as $key => $value)
+			{
+				if(strpos($value, '=') !== false)
+				{
+					$split = explode('=', $value);
+					array_push($temp, $split[0]);
+				}
+				else
+				{
+					array_push($temp, $value);
+				}
+			}
+		}
+		else
+		{
+			$breadcrumb = [];
+		}
+
+		$new_url = $breadcrumb;
+		return $new_url;
+	}
+
+
+	private static function _module()
+	{
+		$module = \lib\url::dir();
+		if(isset($module[0]) && $module[0] === \lib\url::content())
+		{
+			unset($module[0]);
+		}
+
+		$module = array_values($module);
+
+		if(is_array($module) && isset($module[0]))
+		{
+			$module = $module[0];
+		}
+		else
+		{
+			$module = null;
+		}
+
+		return $module;
+	}
+
+	private static function _child()
+	{
+		$module = \lib\url::dir();
+		if(isset($module[0]) && $module[0] === \lib\url::content())
+		{
+			unset($module[0]);
+		}
+
+		$module = array_values($module);
+
+		if(is_array($module) && isset($module[0]))
+		{
+			$module = $module[0];
+		}
+		else
+		{
+			$module = null;
+		}
+
+		if(isset($module[0]) && $module[0] === $module)
+		{
+			unset($module[0]);
+		}
+
+		$module = array_values($module);
+
+		if(is_array($module) && isset($module[0]))
+		{
+			$child = $module[0];
+		}
+		else
+		{
+			$child = null;
+		}
+
+		return $child;
+
+	}
+
+	private static function _tags()
+	{
+
+	}
+
+	private static function _cats()
+	{
+
+	}
+
+	private static function _pages()
+	{
+
+	}
+
+	private static function _LoginService()
+	{
+		$new_url = null;
+		$new_url .= \lib\url::protocol(). '://';
+		$new_url .= \lib\url::domain(). '/account';
+		return $new_url;
+	}
+
+	private static function _account()
+	{
+		$new_url = null;
+		$new_url .= \lib\url::protocol(). '://';
+		$new_url .= \lib\url::domain(). '/account';
+		return $new_url;
+	}
+
+	private static function _MainService()
+	{
+		$new_url = \lib\url::protocol(). '://ermile.'. \lib\url::tld();
+		return $new_url;
 	}
 
 
