d8576f9584b45065c6c7b92ac925473a0b756344
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-03-18 03:41:00 +0330

    add header lib

--- lib/engine/error_page.php
+++ lib/engine/error_page.php
@@ -2,21 +2,36 @@
 <html>
 <head>
  <meta charset="UTF-8"/>
- <title>(>_<)</title>
+ <title>(>_<?php echo $_code;?>_<)</title>
  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"/>
  <link rel="shortcut icon"    href="/static/images/favicon-error.png"/>
- <style type="text/css">*{font-family:sans-serif}body{width:90%;height:94%;margin:0;padding:3% 5%;background-color:#3D5DFF;color:#fff}a{color:#eee;}.slash{padding: 0 2px;}ol{direction:ltr;font-size: 14px;}li{padding-bottom:5px}.addr{font-size: 11px; font-weight: normal;}#no{z-index:-1;position:absolute;bottom:5%;right:10%;opacity:0.3;line-height:50vh;font-size:100px;font-size:20vw}#smile{font-size:7em}</style>
+ <style type="text/css">
+	*{font-family:sans-serif;box-sizing:border-box;}
+	html{min-height:100%;text-rendering:optimizeLegibility;-webkit-font-smoothing:antialiased;}
+	body{width:90%;margin:0 auto;padding:3% 0;background-color:#0e5860;color:#fff}
+	body.s50{background-color:#750700;}
+	a{color:#eee;}
+	.slash{padding:0 2px;}
+	ol{direction:ltr;font-size:14px;}
+	li{padding-bottom:5px}
+	.addr{font-size:11px; font-weight:normal;}
+	#no{z-index:-1;position:absolute;bottom:5%;right:5%;opacity:0.3;line-height:1;font-size:100px;font-size:20vw;animation:pulse 2s infinite ease-in-out;user-select:none;}
+	#smile{font-size:7em}
+	.site{display:inline-block;margin-top:3em;user-select:none;}
+	@keyframes pulse { 0%{opacity:.3} 50%{opacity:.5} 100%{opacity:.3}}
+</style>
 </head>
-<body>
- <h1><?php echo $HTTP_ERROR?></h1>
- <b class='slash'><?php echo $subtitle; ?></b>
+<body class='s<?php echo(substr($_code,0,2));?>'>
+
+ <h1><?php echo $desc?></h1>
+ <b class='slash'><?php echo $_title; ?></b>
 <?php if(defined("DEBUG") && DEBUG && \lib\url::isLocal()  ) {?>
  <ol>
 <?php
-$obj = array_reverse($obj);
-foreach ($obj as $key => $value):?>
+$debug_backtrace = array_reverse($debug_backtrace);
+foreach ($debug_backtrace as $key => $value):?>
 <?php
-  $fileaddr = isset($obj[$key]['file'])? $obj[$key]['file']: null;
+  $fileaddr = isset($debug_backtrace[$key]['file'])? $debug_backtrace[$key]['file']: null;
   if($fileaddr)
   {
   	$fileaddr = substr($fileaddr ,mb_strlen(core)-mb_strlen(core_name)-1);
@@ -24,16 +39,15 @@ foreach ($obj as $key => $value):?>
     $FILE = str_replace("/", "<span class='slash'>/</span>", $FILE);
     $FILE = preg_replace("/([^\/<>]+)$/", "</span>$1", $FILE);
 ?>
-   <li><?php echo $FILE.": Line ".$obj[$key]['line'];?></li>
+   <li><?php echo $FILE.": Line ".$debug_backtrace[$key]['line'];?></li>
 <?php } ?>
 <?php endforeach; ?>
   </ol>
 <?php } else {?>
  <div id="smile">:(</div>
 <?php } ?>
- <br/><br/><br/>
 
- <a href="<?php echo \lib\url::site(); ?>">Return to Homepage</a>
+ <a href="<?php echo \lib\url::site(); ?>" class='site'>Return to Homepage</a>
 
  <div id="no"><?php echo $_code?></div>
 </body>
---
+++ lib/header.php
@@ -0,0 +1,124 @@
+<?php
+namespace lib;
+
+class header
+{
+	/**
+	 * Retrieve the description for the HTTP status
+	 * @param int $_code HTTP status code
+	 * @return string Empty string if not found, or description if found
+	 */
+	public static function desc($_code)
+	{
+		$headers_list =
+		[
+			100 => 'Continue',
+			101 => 'Switching Protocols',
+			102 => 'Processing',
+
+			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authoritative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+			207 => 'Multi-Status',
+			226 => 'IM Used',
+
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			306 => 'Reserved',
+			307 => 'Temporary Redirect',
+			308 => 'Permanent Redirect',
+
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			410 => 'Gone',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entity Too Large',
+			414 => 'Request-URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Requested Range Not Satisfiable',
+			417 => 'Expectation Failed',
+			418 => 'I\'m a teapot',
+			421 => 'Misdirected Request',
+			422 => 'Unprocessable Entity',
+			423 => 'Locked',
+			424 => 'Failed Dependency',
+			426 => 'Upgrade Required',
+			428 => 'Precondition Required',
+			429 => 'Too Many Requests',
+			431 => 'Request Header Fields Too Large',
+			451 => 'Unavailable For Legal Reasons',
+
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout',
+			505 => 'HTTP Version Not Supported',
+			506 => 'Variant Also Negotiates',
+			507 => 'Insufficient Storage',
+			510 => 'Not Extended',
+			511 => 'Network Authentication Required',
+		];
+
+		if(isset($headers_list[$_code]))
+		{
+			return $headers_list[$_code];
+		}
+		else
+		{
+			return '';
+		}
+	}
+
+
+	/**
+	 * Set HTTP status header.
+	 * @param int    $_code       new HTTP status code
+	 */
+	public static function status($_code, $_title = null)
+	{
+		$desc = self::desc($_code);
+		if(empty($desc))
+		{
+			return false;
+		}
+
+		$status_header = "HTTP/1.1 $_code $desc";
+		// set header
+		@header($status_header, true, $_code);
+
+		if(\lib\request::json_accept() || \lib\temp::get('api'))
+		{
+			@header('Content-Type: application/json');
+
+			\lib\notif::title($desc);
+			\lib\notif::error($_title, $_code, "HTTP");
+			echo \lib\notif::compile(true);
+		}
+		else
+		{
+			$debug_backtrace = debug_backtrace(true);
+			require_once(lib."engine/error_page.php");
+		}
+
+		\lib\code::exit();
+	}
+}
+?>
