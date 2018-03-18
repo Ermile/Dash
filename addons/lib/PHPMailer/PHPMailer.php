560627c79496c225156173b72174e61e2f971596
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-03-19 01:23:19 +0330

    relocate phpmailer

--- addons/lib/PHPMailer/PHPMailer.php
+++ addons/lib/PHPMailer/PHPMailer.php
@@ -1971,27 +1971,51 @@ class PHPMailer
         }
 
         // Define full set of translatable strings in English
-        $PHPMAILER_LANG = [
-            'authenticate' => 'SMTP Error: Could not authenticate.',
-            'connect_host' => 'SMTP Error: Could not connect to SMTP host.',
-            'data_not_accepted' => 'SMTP Error: data not accepted.',
-            'empty_message' => 'Message body empty',
-            'encoding' => 'Unknown encoding: ',
-            'execute' => 'Could not execute: ',
-            'file_access' => 'Could not access file: ',
-            'file_open' => 'File Error: Could not open file: ',
-            'from_failed' => 'The following From address failed: ',
-            'instantiate' => 'Could not instantiate mail function.',
-            'invalid_address' => 'Invalid address: ',
-            'mailer_not_supported' => ' mailer is not supported.',
-            'provide_address' => 'You must provide at least one recipient email address.',
-            'recipients_failed' => 'SMTP Error: The following recipients failed: ',
-            'signing' => 'Signing Error: ',
-            'smtp_connect_failed' => 'SMTP connect() failed.',
-            'smtp_error' => 'SMTP server error: ',
-            'variable_set' => 'Cannot set or reset variable: ',
-            'extension_missing' => 'Extension missing: ',
-        ];
+		$PHPMAILER_LANG =
+		[
+			'authenticate'         => 'SMTP Error: Could not authenticate.',
+			'connect_host'         => 'SMTP Error: Could not connect to SMTP host.',
+			'data_not_accepted'    => 'SMTP Error: data not accepted.',
+			'empty_message'        => 'Message body empty',
+			'encoding'             => 'Unknown encoding: ',
+			'execute'              => 'Could not execute: ',
+			'file_access'          => 'Could not access file: ',
+			'file_open'            => 'File Error: Could not open file: ',
+			'from_failed'          => 'The following From address failed: ',
+			'instantiate'          => 'Could not instantiate mail function.',
+			'invalid_address'      => 'Invalid address: ',
+			'mailer_not_supported' => ' mailer is not supported.',
+			'provide_address'      => 'You must provide at least one recipient email address.',
+			'recipients_failed'    => 'SMTP Error: The following recipients failed: ',
+			'signing'              => 'Signing Error: ',
+			'smtp_connect_failed'  => 'SMTP connect() failed.',
+			'smtp_error'           => 'SMTP server error: ',
+			'variable_set'         => 'Cannot set or reset variable: ',
+			'extension_missing'    => 'Extension missing: ',
+		];
+
+		// en text for translation, after set, remove them
+		// $PHPMAILER_LANG['authenticate']         = 'خطای SMTP: احراز هویت با شکست مواجه شد.';
+		// $PHPMAILER_LANG['connect_host']         = 'خطای SMTP: اتصال به سرور SMTP برقرار نشد.';
+		// $PHPMAILER_LANG['data_not_accepted']    = 'خطای SMTP: داده‌ها نا‌درست هستند.';
+		// $PHPMAILER_LANG['empty_message']        = 'بخش متن پیام خالی است.';
+		// $PHPMAILER_LANG['encoding']             = 'کد‌گذاری نا‌شناخته: ';
+		// $PHPMAILER_LANG['execute']              = 'امکان اجرا وجود ندارد: ';
+		// $PHPMAILER_LANG['file_access']          = 'امکان دسترسی به فایل وجود ندارد: ';
+		// $PHPMAILER_LANG['file_open']            = 'خطای File: امکان بازکردن فایل وجود ندارد: ';
+		// $PHPMAILER_LANG['from_failed']          = 'آدرس فرستنده اشتباه است: ';
+		// $PHPMAILER_LANG['instantiate']          = 'امکان معرفی تابع ایمیل وجود ندارد.';
+		// $PHPMAILER_LANG['invalid_address']      = 'آدرس ایمیل معتبر نیست: ';
+		// $PHPMAILER_LANG['mailer_not_supported'] = ' mailer پشتیبانی نمی‌شود.';
+		// $PHPMAILER_LANG['provide_address']      = 'باید حداقل یک آدرس گیرنده وارد کنید.';
+		// $PHPMAILER_LANG['recipients_failed']    = 'خطای SMTP: ارسال به آدرس گیرنده با خطا مواجه شد: ';
+		// $PHPMAILER_LANG['signing']              = 'خطا در امضا: ';
+		// $PHPMAILER_LANG['smtp_connect_failed']  = 'خطا در اتصال به SMTP.';
+		// $PHPMAILER_LANG['smtp_error']           = 'خطا در SMTP Server: ';
+		// $PHPMAILER_LANG['variable_set']         = 'امکان ارسال یا ارسال مجدد متغیر‌ها وجود ندارد: ';
+		// //$PHPMAILER_LANG['extension_missing']    = 'Extension missing: ';
+
+
         if (empty($lang_path)) {
             // Calculate an absolute path so it can work if CWD is not here
             $lang_path = __DIR__ . DIRECTORY_SEPARATOR . 'language' . DIRECTORY_SEPARATOR;
