2d09ac9f953f82de65973f3993da56e1c91c8cc3
Reza <rm.biqarar@gmail.com>
2018-03-19 15:25:14 +0330

    fix crontab dir

--- lib/engine/cronjob/options.php
+++ lib/engine/cronjob/options.php
@@ -36,8 +36,8 @@ class options
 			$new_crontab_txt = str_replace(self::current_cronjob_path(). PHP_EOL, '', $new_crontab_txt);
 		}
 
-		file_put_contents('/tmp/crontab.txt', $new_crontab_txt);
-		exec('crontab /tmp/crontab.txt', $result, $return_val);
+		file_put_contents(__DIR__.'/crontab.txt', $new_crontab_txt);
+		exec('crontab '. __DIR__.'/crontab.txt', $result, $return_val);
 		if($return_val === 0)
 		{
 			return true;
