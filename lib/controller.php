3c0b2b2d16024d0066a532fb620172985ebfe551
Reza <rm.biqarar@gmail.com>
2018-03-19 10:53:47 +0330

    fix ref on controller to app\ref

--- lib/controller.php
+++ lib/controller.php
@@ -97,8 +97,6 @@ class controller
 				break;
 		}
 
-		// save referer of users
-		self::save_ref();
 		// check if isset remember me and login by this
 		\lib\user::check_remeber_login();
 		// redirect
@@ -508,78 +506,6 @@ class controller
 	{
 		$new_url = \lib\url::{$_type}();
 		return $new_url;
-	}
-
-	public static function save_ref()
-	{
-		// it the user is not login and use from ref in url
-		// plus click ref of the referer user
-		if(\lib\request::get("ref") && !\lib\user::login())
-		{
-			$url_ref = \lib\request::get('ref');
-			$url_ref = \lib\coding::decode($url_ref);
-
-			if(!$url_ref)
-			{
-				// invalid ref
-				// fake ref
-				return;
-			}
-			// plus the referer counter click
-			$plus_counter_click = false;
-
-			$log_meta =
-			[
-				'data' => $url_ref,
-				'meta' =>
-				[
-					'url'     => \lib\url::directory(),
-					'ref'     => \lib\request::get(),
-					'session' => $_SESSION,
-				],
-			];
-
-			if(isset($_SESSION['ref']))
-			{
-				if(intval($_SESSION['ref']) === intval($url_ref))
-				{
-					// user pres the F5 :|
-					// neeed less to plus the click counter
-					$plus_counter_click = false;
-				}
-				else
-				{
-					// user change the ref
-					\lib\db\logs::set('user:ref:changed', null, $log_meta);
-					$plus_counter_click = true;
-				}
-			}
-			else
-			{
-				$plus_counter_click = true;
-			}
-
-			$_SESSION['ref'] = $url_ref;
-
-			if($plus_counter_click)
-			{
-				$check_user_exist = \lib\db\users::get(['id' => $url_ref, 'limit' => 1]);
-				if(isset($check_user_exist['id']))
-				{
-					$where =
-					[
-						'user_id' => $check_user_exist['id'],
-						'key'     => 'user_ref_'. (string) $check_user_exist['id'],
-					];
-					\lib\db\options::plus($where);
-				}
-				else
-				{
-					unset($_SESSION['ref']);
-					\lib\db\logs::set('user:ref:referer:not:exist', null, $log_meta);
-				}
-			}
-		}
 	}
 }
 ?>
