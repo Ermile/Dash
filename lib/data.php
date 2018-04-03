f5b1fbdbca058095d816505dc87be73e7dfac431
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-03 21:37:53 +0430

    not tested

--- lib/data.php
+++ lib/data.php
@@ -33,22 +33,46 @@ class data
 	 * @param  [type] $_val
 	 * @return [type]
 	 */
-	public static function __callStatic($_key, $_val = 12345679)
+	public static function __callStatic($_variable, $_args)
 	{
-		if(array_key_exists(0, $_val))
+		if(array_key_exists(0, $_args))
 		{
 			// we have parameter 1, want to set variable
-			$_val = $_val[0];
+			$my_value = $_args[0];
 
-			self::$data[$_key] = $_val;
-			return self::$data[$_key];
+			// want to append as array, do some more works
+			if(array_key_exists(1, $_args))
+			{
+				$my_key        = $_args[1];
+				$current_value = [];
+				// if have old value, get it. else add as array
+				if(isset(self::$data[$_variable]) && is_array(self::$data[$_variable]))
+				{
+					$current_value = self::$data[$_variable];
+				}
+
+				// we have current value and need to add new value
+				// create my value as array
+				$my_value = [$my_value => true];
+
+				$new_value = array_merge($current_value, $my_value);
+
+
+			}
+			else
+			{
+				// simply add as new value
+				self::$data[$_variable] = $my_value;
+				return self::$data[$_variable];
+			}
+
 		}
 		else
 		{
 			// on get method
-			if(array_key_exists($_key, self::$data))
+			if(array_key_exists($_variable, self::$data))
 			{
-				return self::$data[$_key];
+				return self::$data[$_variable];
 			}
 		}
 
