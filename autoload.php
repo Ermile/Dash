f120f2dc151fa31991f115b12cec8e7a0eac80ad
Reza <rm.biqarar@gmail.com>
2018-04-06 20:42:36 +0430

    new autoload

--- autoload.php
+++ autoload.php
@@ -5,130 +5,87 @@
 require_once (__DIR__.'/lib/engine/define.php');
 
 
-/*
-**	In object-oriented applications one of the biggest annoyances is having to write a long list of needed includes
-**	at the beginning of each script.
-**	an __autoload() function automatically called in case you are trying to use a class/interface which hasn't been defined yet.
-**	By calling this function the scripting engine is given a last chance to load the class before PHP fails with an error.
-*/
 class autoload
 {
-	public static $require     = [];
-	public static $core_prefix = ['lib', 'mvc', 'addons'];
-	public static $autoload    = false;
+	private static $required     = [];
 
-	/**
-	 * [load description]
-	 * @param  [type] $name [description]
-	 * @return [type]       [description]
-	 */
-	public static function load($name)
+
+	public static function load($_class_name)
 	{
-		if(isset(self::$require[$name]))
-		{
-			return;
-		}
-		if(strpos($name, 'Twig') === 0)
+		if(isset(self::$required[$_class_name]))
 		{
 			return;
 		}
 
-		$split_name = preg_split("[\\\]", $name);
-		if(count($split_name) > 1)
+		$addr = null;
+
+		if(substr($_class_name, 0, 4) === 'dash')
 		{
-			$file_addr = self::get_file_name($split_name);
-			if($file_addr !== false)
+			$addr = core. 'lib';
+			$addr = $addr. str_replace('dash', '', $_class_name);;
+			$addr = self::os_path($addr);
+			$addr = $addr. '.php';
+
+			if(self::open($addr))
 			{
-				self::$require[$name] = 1;
-				$file_addr = stream_resolve_include_path($file_addr);
-				include_once($file_addr);
+				self::$required[$_class_name] = true;
+			}
+		}
+		elseif(substr($_class_name, 0, 7) === 'content')
+		{
+			$addr = root. $_class_name;
+			$addr = self::os_path($addr);
+			$addr = $addr. '.php';
+
+			if(self::open($addr))
+			{
+				self::$required[$_class_name] = true;
 			}
 			else
 			{
-				$name = preg_replace("/[\\\]/", DIRECTORY_SEPARATOR, $name).'.php';
-				$file_addr = stream_resolve_include_path($name);
-				if($file_addr)
+				$addr = addons. $_class_name;
+				$addr = self::os_path($addr);
+				$addr = $addr. '.php';
+				if(self::open($addr))
 				{
-					self::$require[$name] = 1;
-					include_once($file_addr);
+					self::$required[$_class_name] = true;
 				}
-				else
-				{
-					return false;
-				}
+			}
+		}
+		elseif(substr($_class_name, 0, 3) === 'lib')
+		{
+			$addr = root. 'includes/'. $_class_name;
+			$addr = self::os_path($addr);
+			$addr = $addr. '.php';
+			if(self::open($addr))
+			{
+				self::$required[$_class_name] = true;
 			}
 		}
 	}
 
-	/**
-	 * [get_file_name description]
-	 * @param  [type] $split_name [description]
-	 * @return [type]             [description]
-	 */
-	public static function get_file_name($split_name)
+
+	private static function open($_addr)
 	{
-		list($prefix, $sub_path, $exec_file) = self::file_splice($split_name);
-		$prefix_file = null;
-		if (preg_grep("/^$prefix$/", self::$core_prefix))
+		if(is_file($_addr))
 		{
-			$file_addr = self::check_file($prefix, $sub_path, $exec_file);
-			return $file_addr;
-		}
-
-		$prefix_file = \lib\engine\content::get_addr();
-		$prefix_file = preg_replace("#\/[^\/]+\/?$#", '', $prefix_file);
-		$file_addr   = $prefix_file. '/'. $prefix.'/'. $sub_path. $exec_file;
-		if(!file_exists($file_addr))
-		{
-			$file_addr = false;
-		}
-		return $file_addr;
-	}
-
-
-	/**
-	 * [check_file description]
-	 * @param  [type] $prefix    [description]
-	 * @param  [type] $sub_path  [description]
-	 * @param  [type] $exec_file [description]
-	 * @return [type]            [description]
-	 */
-	public static function check_file($prefix, $sub_path, $exec_file)
-	{
-		if(!defined($prefix))
-		{
-			return false;
-		}
-		$prefix_file = constant($prefix);
-		$file_addr   = $prefix_file .$sub_path .$exec_file;
-		if(stream_resolve_include_path($file_addr))
-		{
-			return $file_addr;
+			include_once($_addr);
+			return true;
 		}
 		return false;
 	}
 
 
-	/**
-	 * [file_splice description]
-	 * @param  [type] $split_name [description]
-	 * @return [type]             [description]
-	 */
-	public static function file_splice($split_name)
+	private static function os_path($_addr)
 	{
-		$prefix = $split_name[0];
-		array_shift($split_name);
-
-		$exec_file = end($split_name);
-		array_pop($split_name);
-
-		$sub_path = (count($split_name) > 0) ? join($split_name, "/") .'/' : '';
-
-		return [$prefix, $sub_path, $exec_file .".php"];
+		$_addr = str_replace('\\', DIRECTORY_SEPARATOR, $_addr);
+		$_addr = str_replace('/', DIRECTORY_SEPARATOR, $_addr);
+		return $_addr;
 	}
 }
+
 spl_autoload_register("\autoload::load");
 
 // LAUNCH DASH!
-\lib\engine\power::on();
+\dash\engine\power::on();
 ?>
