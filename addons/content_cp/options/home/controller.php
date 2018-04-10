f4ff09e92b4ffbc1296c01469bbf4ce2bbd5ff2f
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-10 18:20:37 +0430

    fix co options

--- addons/content_cp/options/home/controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace content_cp\options\home;
-
-class controller extends \addons\content_cp\main\controller
-{
-
-}
-?>
--- addons/content_cp/options/home/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace content_cp\options\home;
-
-class view extends \addons\content_cp\main\view
-{
-
-}
-?>
