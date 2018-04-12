2963cde5bfcf6d0dcf8972f8c8ffe0c9fee6e16e
Reza <rm.biqarar@gmail.com>
2018-04-12 17:55:54 +0430

    fix home page of account

--- addons/content_account/home/model.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace content_account\home;
-
-
-class model extends \content_account\main\model
-{
-
-}
-?>
--- addons/content_account/home/view.php
+++ addons/content_account/home/view.php
@@ -1,26 +1,13 @@
 <?php
 namespace content_account\home;
 
-class view extends \content_account\main\view
+class view
 {
-	/**
-	 * title and desc of page
-	 */
-	public function config()
+
+	public static function config()
 	{
-		$this->data->page['title'] = T_("Dashboard");
-		$this->data->page['desc']  = T_("Account of your user to change profile details or change billing information");
-	}
-
-
-	/**
-	 * view all team and branch
-	 *
-	 * @param      <type>  $_args  The arguments
-	 */
-	public function view_dashboard($_args)
-	{
-
+		\dash\data::page_title(T_("Dashboard"));
+		\dash\data::page_desc(T_("Account of your user to change profile details or change billing information"));
 	}
 }
 ?>
