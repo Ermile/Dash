487080bc8789b1e799ed2329b744f55a539e6c8b
Reza <rm.biqarar@gmail.com>
2018-04-12 19:42:37 +0430

    fix session page

--- addons/content_su/session/model.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace content_su\session;
-
-class model extends \addons\content_su\main\model
-{
-
-}
-?>
--- addons/content_su/session/view.php
+++ addons/content_su/session/view.php
@@ -2,12 +2,11 @@
 namespace content_su\session;
 
 
-class view extends \addons\content_su\main\view
+class view
 {
-	public function config()
+	public static function config()
 	{
-		parent::config();
-		$this->data->my_session = $_SESSION;
+		\dash\data::session($_SESSION);
 	}
 }
 ?>
