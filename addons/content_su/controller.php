54ab0d7ded018856825e1c5ae395b08bc8fd6793
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-17 01:08:52 +0430

    transfer install to new module

--- addons/content_su/controller.php
+++ addons/content_su/controller.php
@@ -3,41 +3,21 @@ namespace content_su;
 
 class controller
 {
-
-	/**
-	 * check if not installed database
-	 * install databse
-	 */
 	public static function routing()
 	{
-		self::install_dash();
 		self::_permission();
 	}
-
-
-	private static function install_dash()
-	{
-		// run if get is set and no database exist
-		if(\dash\url::module() === 'install' && \dash\request::get('time') == 'first_time')
-		{
-			if(!\dash\db::count_table())
-			{
-				require_once(lib."engine/install.php");
-				// this code exit the code
-				\dash\code::end();
-			}
-			else
-			{
-				\dash\header::status(404, T_("System was installed!"));
-			}
-		}
-	}
-
 
 
 	public static function _permission()
 	{
 		// if user is not login then redirect
+		if(\dash\url::module() === 'install' && \dash\request::get('time') == 'first_time')
+		{
+			// turn routing on and allow to check in module
+			return false;
+		}
+
 		if(!\dash\user::login())
 		{
 			\dash\redirect::to(\dash\url::base(). '/enter');
--- addons/content_su/dbtables/model.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace content_su\db;
-
-class model extends \addons\content_su\main\model
-{
-
-}
-?>
