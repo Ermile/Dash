fdceb41706a6f0460a732d3f2b4125e8a823dec7
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-23 14:36:50 +0430

    show database current version in su/update

--- addons/content_su/update/view.php
+++ addons/content_su/update/view.php
@@ -26,6 +26,9 @@ class view
 		\dash\data::dash_version(\dash\engine\version::get());
 		\dash\data::dash_lastUpdate(\dash\utility\git::getLastUpdate());
 		\dash\data::dash_commitCount(\dash\utility\git::getCommitCount());
+
+		\dash\data::dash_dbVersion(\dash\db::db_version());
+		\dash\data::dash_dbVersionAddon(\dash\db::db_version(true, true));
 	}
 }
 ?>
