b2570f0347d3bd382df406a89c4841211f02d4a2
Reza <rm.biqarar@gmail.com>
2018-04-28 08:39:06 +0430

    add cp backup to zip project

--- addons/content_cp/backup/model.php
+++ addons/content_cp/backup/model.php
@@ -5,27 +5,30 @@ class model
 {
 	public static function post()
 	{
-		\dash\notif::warn(T_("Not ready"));
-
-		// if(\dash\request::post('backup') === 'now')
-		// {
-		// 	if(self::backup_db())
-		// 	{
-		// 		self::backup_project();
-		// 	}
-		// }
-		// else
-		// {
-		// 	\dash\notif::ok(T_("Dont!"));
-		// 	return false;
-		// }
+		if(\dash\request::post('backup') === 'now')
+		{
+			if(self::backup_db())
+			{
+				self::backup_project();
+			}
+			else
+			{
+				\dash\notif::ok(T_("Can not create backup"));
+			}
+			\dash\redirect::pwd();
+		}
+		else
+		{
+			\dash\notif::ok(T_("Dont!"));
+			return false;
+		}
 	}
+
 
 	private static function backup_db($_db_name = null)
 	{
 		if(\dash\db::backup_dump(['download' => false, 'db_name' => $_db_name]))
 		{
-			\dash\notif::ok(T_("Database Backup complete"));
 			return true;
 		}
 		return false;
@@ -34,9 +37,36 @@ class model
 
 	private static function backup_project()
 	{
-		$x = \dash\utility\zip::create('/home/reza/1.zip', root);
-		// var_dump($x);exit();
+		self::clean_old();
+
+		$zip_addr = \content_cp\backup\controller::backup_addr();
+		\dash\file::makeDir($zip_addr, null, true);
+
+
+		$file_name = "Backup_". date("Y_m_d_H_i_"). \dash\url::root(). '.zip';
+
+		$zip = \dash\utility\zip::folder($zip_addr. $file_name, root);
+		if($zip)
+		{
+			\dash\notif::ok(T_("Backup Complete"));
+		}
+		else
+		{
+			\dash\notif::ok(T_("Can not create backup"));
+		}
 	}
 
+
+	private static function clean_old()
+	{
+		$oldBackup = @glob(\content_cp\backup\controller::backup_addr().'*');
+		if($oldBackup && is_array($oldBackup))
+		{
+			foreach ($oldBackup as $key => $value)
+			{
+				unlink($value);
+			}
+		}
+	}
 }
 ?>
