af29fd35f319a1ec03c6621e3f02de80fc845752
Reza <rm.biqarar@gmail.com>
2018-04-28 18:19:58 +0430

    load backup for some project if su

--- addons/content_cp/backup/controller.php
+++ addons/content_cp/backup/controller.php
@@ -5,7 +5,14 @@ class controller
 {
 	public static function routing()
 	{
-
+		if(\dash\option::config('full_backup') || \dash\permission::access_su())
+		{
+			// no problem
+		}
+		else
+		{
+			\dash\header::status(403);
+		}
 	}
 }
 ?>
