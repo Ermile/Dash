7d9ebe63c9e8bb11f17bd721827c06705612e772
Reza <rm.biqarar@gmail.com>
2018-04-28 11:11:03 +0430

    fix backup icon

--- addons/content_cp/backup/view.php
+++ addons/content_cp/backup/view.php
@@ -15,16 +15,21 @@ class view
 		{
 			foreach ($oldBackup as $key => $value)
 			{
+				$zip = false;
+				if(substr(basename($value), 0, 7) === 'Backup_')
+				{
+					$zip = true;
+				}
 				$oldBackup_files [] =
 				[
+					'zip'  => $zip,
 					'name' => basename($value),
 					'time' => filemtime($value),
 					'size' => round(filesize($value) / 1024 / 1024, 1),
 					'date' => date("Y-m-d H:i:s", filemtime($value)),
-					'ago' => \dash\utility\human::timing(date("Y-m-d H:i:s", filemtime($value))),
+					'ago'  => \dash\utility\human::timing(date("Y-m-d H:i:s", filemtime($value))),
 				];
 			}
-			$oldBackup_files = array_reverse($oldBackup_files);
 			\dash\data::oldBackup($oldBackup_files);
 		}
 
