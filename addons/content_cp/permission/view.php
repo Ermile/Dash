a4405d95fdd55e4fe72281939983291b5f8f8552
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-06 15:39:27 +0430

    add first sample of permissions

---
+++ addons/content_cp/permission/view.php
@@ -0,0 +1,62 @@
+<?php
+namespace content_cp\permission;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_("Permissions"));
+		\dash\data::page_desc(T_("Set and config permission of users and allow them to do something."));
+
+		\dash\data::perm_group(
+			[
+				'admin'=> ['title' => 'مدیر کل'],
+				'editor'=> ['title' => 'ویراستار'],
+				'writer'=> ['title' => 'نویسنده'],
+			]
+		);
+		\dash\data::perm_list(
+			[
+				'cp:news:view'=>
+				[
+					'title' => 'مشاهده خبر',
+					'cat' => 'cp',
+					'subcat' => 'news',
+					'check' => false,
+					'verify' => false,
+					'require' => null,
+				],
+				'cp:news:add'=>
+				[
+					'title' => 'افزودن خبر',
+					'cat' => 'cp',
+					'subcat' => 'news',
+					'check' => false,
+					'verify' => false,
+					'require' => ['cp:news:view'],
+				],
+				'cp:news:edit'=>
+				[
+					'title' => 'ویرایش خبر',
+					'cat' => 'cp',
+					'subcat' => 'news',
+					'check' => false,
+					'verify' => false,
+					'require' => ['cp:news:view'],
+				],
+				'cp:news:delete'=>
+				[
+					'title' => 'حذف خبر',
+					'cat' => 'cp',
+					'subcat' => 'news',
+					'check' => false,
+					'verify' => false,
+					'require' => ['cp:news:view'],
+				],
+			]
+		);
+
+	}
+}
+?>
