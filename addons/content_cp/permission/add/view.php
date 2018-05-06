8753a9e830c8f12658864732cb21018e20506951
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-06 16:05:53 +0430

    add perm add start page

---
+++ addons/content_cp/permission/add/view.php
@@ -0,0 +1,41 @@
+<?php
+namespace content_cp\permission\add;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_("Permissions"));
+		\dash\data::page_desc(T_("Set and config permission of users and allow them to do something."));
+
+
+		\dash\data::perm_list(
+			[
+				'news'=>
+				[
+					'cp:news:view' =>
+					[
+						'title' => 'مشاهده خبر',
+						'cat' => 'cp',
+						'subcat' => 'news',
+						'check' => false,
+						'verify' => false,
+						'require' => null,
+					],
+					'cp:news:add'=>
+					[
+						'title' => 'افزودن خبر',
+						'cat' => 'cp',
+						'subcat' => 'news',
+						'check' => false,
+						'verify' => false,
+						'require' => ['cp:news:view'],
+					],
+				]
+			]
+		);
+
+	}
+}
+?>
