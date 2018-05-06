73576bc20881079a5d8af21493debab2bb9fe243
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-06 17:00:36 +0430

    fix badeges

--- addons/content_cp/permission/add/view.php
+++ addons/content_cp/permission/add/view.php
@@ -6,9 +6,11 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_("Permissions"));
-		\dash\data::page_desc(T_("Set and config permission of users and allow them to do something."));
+		\dash\data::page_title(T_("Add new permissions"));
+		\dash\data::page_desc(T_("Set and config permission group to categorize user access."));
 
+		\dash\data::badge_link(\dash\url::this());
+		\dash\data::badge_text(T_('Back to list of permissions'));
 
 		\dash\data::perm_list(
 			[
--- addons/content_cp/permission/view.php
+++ addons/content_cp/permission/view.php
@@ -9,6 +9,10 @@ class view
 		\dash\data::page_title(T_("Permissions"));
 		\dash\data::page_desc(T_("Set and config permission of users and allow them to do something."));
 
+
+		\dash\data::badge_link(\dash\url::this().'/add');
+		\dash\data::badge_text(T_('Add new permission'));
+
 		\dash\data::perm_group(
 			[
 				'admin'=> ['title' => 'مدیر کل'],
