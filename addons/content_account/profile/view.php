dffc030efbe745cefa872ff414c898243df2ef25
Reza <rm.biqarar@gmail.com>
2018-05-12 14:54:38 +0430

    update trans

--- addons/content_account/profile/view.php
+++ addons/content_account/profile/view.php
@@ -8,10 +8,10 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Edit your profile'));
-		\dash\data::page_desc(T_('You can add edit your profile.'));
+		\dash\data::page_desc(T_('You can edit your profile.'));
 
 		\dash\data::badge_link(\dash\url::this());
-		\dash\data::badge_text(T_('Back to dashbaord list'));
+		\dash\data::badge_text(T_('Back to dashbaord'));
 
 		$id = \dash\user::id();
 
