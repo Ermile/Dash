f5614d3a3533e7586cf84d76586ff8f4717260ce
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-23 10:12:34 +0430

    fix edit comment design

--- addons/content_cp/comments/edit/view.php
+++ addons/content_cp/comments/edit/view.php
@@ -18,7 +18,7 @@ class view
 		// $this->data->cat_list              = \dash\app\term::cat_list();
 
 		\dash\data::page_title(T_("Edit comment"));
-		\dash\data::page_desc(T_("You can accept or reject the comment"));
+		\dash\data::page_desc(T_("You can edit comments if needed."). ' '. T_("This is often useful when you notice that a commenter has made a typographical error."));
 
 		\dash\data::badge_link(\dash\url::this());
 		\dash\data::badge_text(T_('Back to list of comments'));
