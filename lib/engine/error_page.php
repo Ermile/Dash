ab8fcbb6eb9a085f52ef4c72b03a9e353ad44e21
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-06-20 18:28:37 +0430

    better error page

--- lib/engine/error_page.php
+++ lib/engine/error_page.php
@@ -6,28 +6,18 @@
  <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><![endif]-->
  <meta name ="viewport" content="width=device-width, initial-scale=1.0, height=device-height, minimum-scale=1.0 maximum-scale=1.5, minimal-ui"/>
  <link rel="shortcut icon" href="/static/images/favicon-error.png"/>
- <style>
-	*{font-family:sans-serif;box-sizing:border-box;}
-	html{min-height:100%;text-rendering:optimizeLegibility;-webkit-font-smoothing:antialiased;}
-	body{width:90%;margin:0 auto;padding:3% 0;background-color:#0e5860;color:#fff}
-	body.s50{background-color:#750700;}
-	a{color:#eee;}
-	.slash{padding:0 2px;}
-	ol{direction:ltr;font-size:14px;}
-	li{padding-bottom:5px}
-	.addr{font-size:11px; font-weight:normal;}
-	#no{z-index:-1;position:absolute;bottom:5%;right:5%;opacity:0.3;line-height:1;font-size:100px;font-size:20vw;animation:pulse 2s infinite ease-in-out;user-select:none;}
-	#smile{font-size:7em}
-	.site{display:inline-block;margin-top:3em;user-select:none;}
-	@keyframes pulse { 0%{opacity:.3} 50%{opacity:.5} 100%{opacity:.3}}
-</style>
+ <!-- <linkss rel="stylesheet"  href="/static/siftal/css/siftal.css"> -->
+ <link rel="stylesheet"  href="http://siftal.local/dist/css/siftal.css">
 </head>
-<body class='s<?php echo(substr($_code,0,2));?>'>
-
- <h1><?php echo $desc?></h1>
- <b class='slash'><?php echo $_text; ?></b>
+<body class='<?php echo (\dash\language::current('direction')); ?> errorPage s<?php echo(substr($_code,0,2));?>'>
+ <div id="nodes"></div>
+ <div class="cn">
+  <div class="wrapper">
+   <h1><?php echo $translatedDesc;?></h1>
+   <h2><?php echo $_text;?></h2>
+   <a href="<?php echo \dash\url::site(); ?>" class='btn secondary'><?php echo T_("Return to Homepage")?></a>
 <?php if(\dash\option::config('debug') || \dash\url::isLocal()) {?>
- <ol>
+   <ol>
 <?php
 $debug_backtrace = array_reverse($debug_backtrace);
 foreach ($debug_backtrace as $key => $value):?>
@@ -35,21 +25,24 @@ foreach ($debug_backtrace as $key => $value):?>
   $fileaddr = isset($debug_backtrace[$key]['file'])? $debug_backtrace[$key]['file']: null;
   if($fileaddr)
   {
-  	$fileaddr = substr($fileaddr ,mb_strlen(core)-mb_strlen(core_name)-1);
-    $FILE = '<span class="addr">'.$fileaddr;
+    $fileaddr = substr($fileaddr ,mb_strlen(core)-mb_strlen(core_name)-1);
+    $FILE = $fileaddr;
     $FILE = str_replace("/", "<span class='slash'>/</span>", $FILE);
-    $FILE = preg_replace("/([^\/<>]+)$/", "</span>$1", $FILE);
+    $FILE = str_replace("\\", "<span class='slash'>/</span>", $FILE);
 ?>
-   <li><?php echo $FILE.": Line ".$debug_backtrace[$key]['line'];?></li>
+     <li><?php echo $FILE.": Line ".$debug_backtrace[$key]['line'];?></li>
 <?php } ?>
 <?php endforeach; ?>
-  </ol>
+    </ol>
 <?php } else {?>
- <div id="smile">:(</div>
+   <div id="smile">:(</div>
 <?php } ?>
 
- <a href="<?php echo \dash\url::site(); ?>" class='site'>Return to Homepage</a>
 
+  </div>
+ </div>
  <div id="no"><?php echo $_code?></div>
+
+ <script src="http://siftal.local/dist/js/error_page.js"></script>
 </body>
 </html>
