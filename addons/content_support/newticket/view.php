73359952eda405150c2259a434c40ab13f9ff0b3
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-07-04 01:15:09 +0430

    add new ticket module

---
+++ addons/content_support/newticket/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content_support\home;
+
+class view
+{
+
+	public static function config()
+	{
+		\dash\data::page_title(T_("Add new ticket"));
+		\dash\data::page_desc(T_("Dot worry!"). ' '. T_("Ask your question."). ' '. T_("We are here to answer your questions."));
+	}
+}
+?>
