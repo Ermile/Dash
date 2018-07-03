a182565f2bf907e4824420453547428227ff7108
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-07-03 13:34:14 +0430

    show error on php 5.6

--- lib/engine/prepare.php
+++ lib/engine/prepare.php
@@ -474,6 +474,10 @@ class prepare
 		// check php version to upper than 7.0
 		if(version_compare(phpversion(), '7.0', '<'))
 		{
+			if(version_compare(phpversion(), '5.6', '<'))
+			{
+				\dash\code::pretty("<p>For using Dash you must update php version to 7.0 or higher!</p>");
+			}
 			\dash\code::bye("<p>For using Dash you must update php version to 7.0 or higher!</p>");
 		}
 	}
