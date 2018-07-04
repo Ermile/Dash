83bd04035b69424242210f8677da4bc6cece88a1
Reza <rm.biqarar@gmail.com>
2018-07-04 13:59:18 +0430

    show list of open ticket in dashboard

--- addons/content_support/home/view.php
+++ addons/content_support/home/view.php
@@ -8,6 +8,24 @@ class view
 	{
 		\dash\data::page_title(T_("Ticketing System"));
 		\dash\data::page_desc(T_("Easily manage your tickets and monitor or track them to get best answer until fix your problem"));
+
+		$args['sort']            = 'id';
+		$args['order']           = 'desc';
+		$args['comments.type']   = 'ticket';
+
+		if(!\dash\permission::check('supportTicketView'))
+		{
+			$args['user_id']         = \dash\user::id();
+		}
+
+		$args['comments.parent'] = null;
+		$args['pagenation']      = false;
+		$args['join_user']       = true;
+		$args['comments.status']       = ["NOT IN", "('close')"];
+
+		$dataTable = \dash\app\comment::list(null, $args);
+
+		\dash\data::dataTable($dataTable);
 	}
 }
 ?>
