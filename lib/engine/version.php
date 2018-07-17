d7cb719ef2b9a21f019c3f320b551ed729f33654
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-07-17 17:44:09 +0430

    show user detail if exist

--- lib/engine/version.php
+++ lib/engine/version.php
@@ -6,7 +6,7 @@ namespace dash\engine;
 class version
 {
 	// @var dash engine current version
-	const version = '15.0.9';
+	const version = '15.1.0';
 
 	// @var dash engine current commit number
 	// now get it automatically from git commands
