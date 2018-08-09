29d144ec6589dbe634fd16e245abc5f47883ed2e
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-09 15:28:29 +0430

    get url from user

--- addons/content_su/tg/sendphoto/model.php
+++ addons/content_su/tg/sendphoto/model.php
@@ -5,14 +5,15 @@ class model
 {
 	public static function post()
 	{
-		$chatid = \dash\request::post('chatid');
-		$text   = \dash\request::post('text');
-		// $file   = \dash\app\file::upload_quick('file1');
-		$file   = \dash\request::files('file1');
-		$file   = file_get_contents(\dash\request::files('file1')['tmp_name']);
+		$chatid   = \dash\request::post('chatid');
+		$text     = \dash\request::post('text');
+		$fileData = "https://ermile.com/static/images/logo.png";
+		$fileData = \dash\app\file::upload_quick('file1');
 
-		$myData   = ['chat_id' => $chatid, 'photo' => $file, 'caption' => $text];
-		$myResult = \dash\social\telegram\tg::sendMessage($myData);
+
+
+		$myData   = ['chat_id' => $chatid, 'photo' => $fileData, 'caption' => $text];
+		$myResult = \dash\social\telegram\tg::sendPhoto($myData);
 
 		\dash\session::set('tg_send', $myData);
 		\dash\session::set('tg_response', $myResult);
