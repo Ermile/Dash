e9fac4ce7d8d2953bff01455640f32e38b0577b5
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-09 14:45:33 +0430

    tg try to show result

--- addons/content_su/tg/sendmessage/model.php
+++ addons/content_su/tg/sendmessage/model.php
@@ -8,11 +8,10 @@ class model
 		$chatid = \dash\request::post('chatid');
 		$text   = \dash\request::post('text');
 
-		// var_dump($chatid);
-		// var_dump($text);
-
 		$myData = ['chat_id' => $chatid, 'text' => $text];
 		$result = \dash\social\telegram\tg::sendMessage($myData);
+
+
 		var_dump($myData);
 		var_dump($result);
 	}
