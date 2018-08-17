1e26403627c3ea74a8a211baf3df8c5422373e62
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-17 19:28:44 +0430

    try to write done log

--- lib/social/telegram/log.php
+++ lib/social/telegram/log.php
@@ -3,6 +3,56 @@ namespace dash\social\telegram;
 
 class log extends tg
 {
+	public static $hookDate    = null;
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * prepare array to save record of log in database
+	 * @return function [description]
+	 */
+	public static function done()
+	{
+		$myDetail =
+		[
+			'chatid'        => hook::from(),
+			'user_id'       => \dash\user::id(),
+			// 'hook'          => '',
+			// 'hookdate'      => '',
+			// 'hooktext'      => '',
+			// 'hookmessageid' => '',
+			// 'sendmethod'    => '',
+			// 'send'          => '',
+			// 'senddate'      => '',
+			// 'sendtext'      => '',
+			// 'sendmesageid'  => '',
+			// 'sendkeyboard'  => '',
+			// 'response'      => '',
+			// 'responsedate'  => '',
+			// 'url'           => '',
+			// 'step'          => '',
+			// 'meta'          => '',
+			// 'status'        => '',
+		];
+
+		// \dash\db\telegrams::insert($myDetail);
+	}
+
+
+
+
+
+
+
+
+
 	public static function save($_method = null, $_sendData = null, $_sendDate = null, $_response = null)
 	{
 		$myDetail =
@@ -59,55 +109,6 @@ class log extends tg
 	}
 
 
-
-
-	/**
-	 * save data on hooking
-	 * @param  [type] $_data [description]
-	 * @return [type]        [description]
-	 */
-	private static function saveHookOld($_data)
-	{
-		// define user detail array
-		$from_id = self::response('from');
-		// if we do not have from id return false
-		if(!isset($_data['message']['from']) || !$from_id)
-		{
-			return false;
-		}
-
-		// catch user telegram from database and if not exist insert as new user
-		self::catchTelegramUser($from_id, $_data['message']['from']);
-
-		// // save user detail like contact or location if sended
-		// if($contact = self::response('contact', null))
-		// {
-		// 	self::saveUserDetail('contact', $contact);
-		// }
-		// elseif($location = self::response('location'))
-		// {
-		// 	self::saveUserDetail('location', $location);
-		// }
-
-
-
-		// // change language if needede
-		// if(\lib\router::get_storage('language') !== self::$language)
-		// {
-		// 	\lib\router::set_storage('language', self::$language );
-		// 	// use saloos php gettext function
-		// 	require_once(lib.'utility/gettext/gettext.inc');
-		// 	// gettext setup
-		// 	T_setlocale(LC_MESSAGES, \lib\router::get_storage('language'));
-		// 	// Set the text domain as 'messages'
-		// 	T_bindtextdomain('messages', root.'includes/languages');
-		// 	T_bind_textdomain_codeset('messages', 'UTF-8');
-		// 	T_textdomain('messages');
-		// }
-		return true;
-	}
-
-
 	/**
 	 * save history of messages into session of this user
 	 * @param  [type] $_text [description]
@@ -137,17 +138,6 @@ class log extends tg
 		}
 	}
 
-
-
-
-
-
-
-
-	// \dash\db\telegrams::get(['key' => 'value', 'key2' => 'value2']);
-    // \dash\db\telegrams::get(['key' => 'value', 'key2' => 'value2', 'limit' => 1]); // return array by size 1
-    // \dash\db\telegrams::insert(['key' => 'value', 'key2' => 'value2']);
-    // \dash\db\telegrams::update(['key' => 'value', 'key2' => 'value2'], 10); // where id = 10 update
 
 }
 ?>
