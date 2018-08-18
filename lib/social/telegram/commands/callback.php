d9324c1a519fcb7a46acc327ecffe8ae11c582b4
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-18 19:15:55 +0430

    add callback class

---
+++ lib/social/telegram/commands/callback.php
@@ -0,0 +1,63 @@
+<?php
+namespace dash\social\telegram\commands;
+// use telegram class as bot
+use \dash\social\telegram\tg as bot;
+
+class callback
+{
+	/**
+	 * execute user request and return best result
+	 * @param  [type] $_cmd [description]
+	 * @return [type]       [description]
+	 */
+	public static function run($_cmd)
+	{
+		$response = null;
+		switch ($_cmd['command'])
+		{
+			case 'cb_go_right':
+				$response = self::go_right();
+				break;
+
+			case 'cb_go_left':
+				$response = self::go_left();
+				break;
+
+			default:
+				break;
+		}
+		if($response)
+		{
+			if(!isset($result['show_alert']))
+			{
+				$response['show_alert'] = true;
+			}
+			bot::answerCallbackQuery($response);
+
+		}
+		return $response;
+	}
+
+
+	/**
+	 *
+	 * @return [type] [description]
+	 */
+	public static function go_right()
+	{
+		$result['text'] = 'hey right';
+		return $result;
+	}
+
+
+	/**
+	 *
+	 * @return [type] [description]
+	 */
+	public static function go_left()
+	{
+		$result['text'] = 'hey left';
+		return $result;
+	}
+}
+?>
