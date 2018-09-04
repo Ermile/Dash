6383e29994a543eda001c7ea3288ae2f46ef8322
Reza <rm.biqarar@gmail.com>
2018-09-04 14:53:49 +0430

    show notifications

--- addons/content_account/notification/view.php
+++ addons/content_account/notification/view.php
@@ -6,9 +6,32 @@ class view
 
 	public static function config()
 	{
-		\dash\notification::send('verificationCode', \dash\user::id(), ['name' => null, 'title' => null]);
+		$args =
+		[
+			'sort'  => \dash\request::get('sort'),
+			'order' => \dash\request::get('order'),
+		];
 
-		\dash\data::appkey(\dash\utility\appkey::get_app_key(\dash\user::id()));
+		if(!$args['order'])
+		{
+			$args['order'] = 'desc';
+		}
+
+		$search_string = \dash\request::get('q');
+
+		$sortLink  = \dash\app\sort::make_sortLink(\lib\app\grade::$sort_field, \dash\url::this());
+		$dataTable = \dash\app\notification::list($search_string, $args);
+
+		\dash\data::sortLink($sortLink);
+		\dash\data::dataTable($dataTable);
+
+		$check_empty_datatable = $args;
+		unset($check_empty_datatable['sort']);
+		unset($check_empty_datatable['order']);
+
+		// set dataFilter
+		$dataFilter = \content_m\view::createFilterMsg($search_string, $check_empty_datatable);
+		\dash\data::dataFilter($dataFilter);
 	}
 }
 ?>
