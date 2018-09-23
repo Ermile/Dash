7db7d4657413e6d90f203ccb56c0a126caea7847
Reza <rm.biqarar@gmail.com>
2018-09-23 14:58:09 +0330

    add support bredcrumb

--- addons/content_support/view.php
+++ addons/content_support/view.php
@@ -18,6 +18,57 @@ class view
 		\dash\data::display_admin('content_support/layout.html');
 
 		\dash\data::maxUploadSize(\dash\utility\upload::max_file_upload_size(true));
+
+		self::make_breadcrumb();
 	}
+
+	public static function make_breadcrumb()
+	{
+
+		$myBreadCrumb = [];
+
+		$myBreadCrumb[] =
+		[
+			"text"  => \dash\data::site_title(),
+			"link"  => \dash\url::sitelang(),
+			'title' => \dash\data::site_desc(),
+			"icon"  => "home",
+			'attr'  => 'data-direct',
+		];
+
+		if(\dash\url::module())
+		{
+			$myBreadCrumb[] =
+			[
+				"text"  => T_("Support"),
+				"link"  => \dash\url::here(),
+				'title' => T_("Support dashboard"),
+			];
+		}
+
+
+		if(\dash\url::child())
+		{
+			switch (\dash\url::child())
+			{
+				case 'show':
+				case 'add':
+				case 'message':
+					$myBreadCrumb[] =
+					[
+						"text"  => T_("Ticket"),
+						"link"  => \dash\url::this(),
+					];
+					break;
+
+				default:
+					# nothing
+					break;
+			}
+		}
+
+		\dash\data::page_breadcrumb($myBreadCrumb);
+	}
+
 }
 ?>
