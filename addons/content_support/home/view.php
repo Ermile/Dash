969b8bd63933347fcd99818ef72450480e40c90f
Reza <rm.biqarar@gmail.com>
2018-09-23 15:12:49 +0330

    fix support page title

--- addons/content_support/home/view.php
+++ addons/content_support/home/view.php
@@ -6,7 +6,7 @@ class view
 
 	public static function config()
 	{
-		\dash\data::page_title(T_("Ticketing System"));
+		\dash\data::page_title(T_("Help center"));
 		\dash\data::page_desc(T_("Easily manage your tickets and monitor or track them to get best answer until fix your problem"));
 		\dash\data::page_pictogram('life-ring');
 
