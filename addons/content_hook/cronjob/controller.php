8992a38a88c5165fd428bfd7e705f0473b103018
Reza <rm.biqarar@gmail.com>
2018-09-24 19:26:45 +0330

    fix cronjob

--- addons/content_hook/cronjob/controller.php
+++ addons/content_hook/cronjob/controller.php
@@ -23,23 +23,23 @@ class controller
 		// 	return;
 		// }
 
-		// if
-		// (
-		// 	preg_match("/^127\\.0\\.0\\.\d+$/", $_SERVER['SERVER_ADDR']) ||
-		// 	(
-		// 		isset($_SERVER['REMOTE_ADDR']) &&
-		// 		isset($_SERVER['SERVER_ADDR']) &&
-		// 		$_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']
-		// 	)
-		// )
-		// {
-		// 	// no thing
-		// }
-		// else
-		// {
-		// 	// \dash\utility\telegram::sendMessage("@tejarak_monitor", "#ERROR\n".  json_encode($_SERVER, JSON_UNESCAPED_UNICODE));
-		// 	\dash\header::status(404);
-		// }
+		if
+		(
+			preg_match("/^127\\.0\\.0\\.\d+$/", $_SERVER['SERVER_ADDR']) ||
+			(
+				isset($_SERVER['REMOTE_ADDR']) &&
+				isset($_SERVER['SERVER_ADDR']) &&
+				$_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']
+			)
+		)
+		{
+			// no thing
+		}
+		else
+		{
+			// \dash\utility\telegram::sendMessage("@tejarak_monitor", "#ERROR\n".  json_encode($_SERVER, JSON_UNESCAPED_UNICODE));
+			\dash\header::status(404);
+		}
 
 	}
 }
