f1bf5b9c4f30f932e83fe7f2a03e5e3763aec2f4
Reza <rm.biqarar@gmail.com>
2018-09-24 09:14:42 +0330

    check tag permission

--- addons/content_support/ticket/show/model.php
+++ addons/content_support/ticket/show/model.php
@@ -45,7 +45,21 @@ class model
 
 			if(!\dash\permission::check('cpTagSupportAdd'))
 			{
-				// check tags to not add new tag
+				$current_tag = \dash\db\terms::get(['type' => 'support_tag']);
+				if(is_array($current_tag))
+				{
+					$tag_titles = array_column($current_tag, 'title');
+					$new_tag    = \dash\request::post('tag');
+					$new_tag    = explode(',', $new_tag);
+					foreach ($new_tag as $key => $value)
+					{
+						if(!in_array($value, $tag_titles))
+						{
+							\dash\notif::error(T_("Please select tag from list"), 'tag');
+							return false;
+						}
+					}
+				}
 			}
 
 			\dash\app::variable(['support_tag' => \dash\request::post('tag')]);
