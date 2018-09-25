0992c040ee1230a2f31b0ff4275c7de9fd7af6c8
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-09-25 16:20:08 +0330

    add get fn to datetime

--- lib/datetime.php
+++ lib/datetime.php
@@ -10,22 +10,22 @@ class datetime
 	 * @param  boolean $_long [description]
 	 * @return [type]         [description]
 	 */
-	public static function format($_type, $_long = true)
+	public static function format($_type = null, $_long = null, $_lang = null)
 	{
 		switch ($_type)
 		{
 			case 'date':
 				switch ($_long)
 				{
-					case 'short':
-						// 2018-09-25
-						return 'Y-m-d';
-						break;
-
-					case 'long':
-					default:
+					case true:
 						// Tuesday 25 September 2018
 						return 'l d F Y';
+						break;
+
+					case false:
+					default:
+						// 2018-09-25
+						return 'Y-m-d';
 						break;
 				}
 				break;
@@ -33,13 +33,15 @@ class datetime
 			case 'time':
 				switch ($_long)
 				{
-					case 'short':
-						return 'H:i';
+					case true:
+						// 4:05:52 PM
+						return 'g:i:s A';
 						break;
 
-					case 'long':
+					case false:
 					default:
-						return 'g:i:s A';
+						// 16:05
+						return 'H:i';
 						break;
 				}
 				break;
@@ -48,13 +50,13 @@ class datetime
 			default:
 				switch ($_long)
 				{
-					case 'short':
-						return 'Y-m-d'. ' '. 'H:i';
+					case true:
+						return 'l d F Y'. ' '. 'H:i:s';
 						break;
 
-					case 'long':
+					case false:
 					default:
-						return 'l d F Y'. ' '. 'g:i:s A';
+						return 'Y-m-d'. ' '. 'H:i';
 						break;
 				}
 				break;
@@ -62,6 +64,55 @@ class datetime
 
 	}
 
+
+	public static function get(
+		$_datetime,
+		$_long = null,
+		$_type = 'datetime',
+		$_lang = null,
+		$_convertNumber = null
+	)
+	{
+		// step1 - check datetime
+
+		// step2 - get new format
+		$myFormat   = self::format($_type, $_long);
+		$myDatetime = strtotime($_datetime);
+		$finalDate  = null;
+		// detect current lang if not set
+		if($_lang === null)
+		{
+			$_lang = \dash\language::current();
+		}
+
+		// step3 - change to new format
+		switch ($_lang)
+		{
+			case 'fa':
+				$finalDate = \dash\utility\jdate::date($myFormat, $myDatetime);
+				break;
+
+			case 'en':
+			default:
+				$finalDate = date($myFormat, $myDatetime);
+				break;
+		}
+
+		// step4 - change number to fa if need
+		if($_convertNumber !== false)
+		{
+			$finalDate = \dash\utility\human::number($finalDate, $_lang);
+		}
+
+
+		return $finalDate;
+	}
+
+
+	public static function fit($_datetime, $_long = null)
+	{
+		return self::get($_datetime, $_long);
+	}
 
 }
 ?>
