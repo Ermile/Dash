bb86a5dcb0c3f2f187ae90137269ff63d45b0ede
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-09-27 15:51:16 +0330

    add highchart for main chart

--- addons/content_cp/view.php
+++ addons/content_cp/view.php
@@ -10,6 +10,7 @@ class view
 		\dash\data::include_css(false);
 		\dash\data::include_js(false);
 		\dash\data::include_chart(true);
+		\dash\data::include_highcharts(true);
 
 
 		\dash\data::include_editor(true);
--- addons/content_cp/visitor/home/chart.js
+++ addons/content_cp/visitor/home/chart.js
@@ -1,11 +1,93 @@
 function chartDrawer()
 {
-  if($("#chartdiv").length == 1){myChart();}
+  if($("#chartdiv").length == 1){highChart();}
 }
 
 
 
-function myChart()
+function highChart()
+{
+
+Highcharts.chart('chartdiv',
+{
+  chart: {
+    zoomType: 'x'
+  },
+  title: {
+    text: '{%trans "Website analytics"%}'
+  },
+  xAxis: [{
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    crosshair: true
+  }],
+  yAxis: [{ // Primary yAxis
+    labels: {
+      format: '{value}',
+      style: {
+        color: Highcharts.getOptions().colors[0]
+      }
+    },
+    title: {
+      text: '{%trans "Page"%}',
+      style: {
+        color: Highcharts.getOptions().colors[0]
+      }
+    }
+  },
+  { // Secondary yAxis
+    title: {
+      text: '{%trans "Person"%}',
+      style: {
+        color: Highcharts.getOptions().colors[1]
+      }
+    },
+    labels: {
+      format: '{value}',
+      style: {
+        color: Highcharts.getOptions().colors[1]
+      }
+    },
+    opposite: true
+  }],
+  tooltip: {
+    shared: true
+  },
+  legend: {
+    layout: 'vertical',
+    align: 'left',
+    x: 120,
+    verticalAlign: 'top',
+    y: 100,
+    floating: true,
+    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
+  },
+  series: [
+  {
+    name: '{%trans "Page view"%}',
+    type: 'column',
+    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+    tooltip: {
+      valueSuffix: ' {%trans "page"%}'
+    }
+
+  },
+  {
+    name: '{%trans "Visit"%}',
+    type: 'spline',
+    yAxis: 1,
+    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+    tooltip: {
+      valueSuffix: ' {%trans "person"%}'
+    }
+  }
+  ]
+});
+}
+
+
+
+function myChart1()
 {
   am4core.useTheme(am4themes_animated);
 
