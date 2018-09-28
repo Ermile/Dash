f414e3f459109689179bf5056a43a69fc74d66cc
Reza <rm.biqarar@gmail.com>
2018-09-28 21:19:38 +0330

    save ticket in tg

--- lib/app/tg/ticket.php
+++ lib/app/tg/ticket.php
@@ -8,6 +8,18 @@ class ticket
 	{
 		// save answer
 
+		// ready to insert comments
+		$args =
+		[
+			'type'    => 'ticket',
+			'content' => $_id,
+			'user_id' => \dash\user::id(true),
+			'parent'  => $_id,
+
+		];
+
+		$result = \dash\app\ticket::add($args);
+
 		return true;
 	}
 }
