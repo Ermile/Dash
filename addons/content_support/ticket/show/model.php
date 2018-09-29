11196dcacb8eeb32e13ff5860b53293626d0cd5b
Reza <rm.biqarar@gmail.com>
2018-09-29 11:20:16 +0330

    ready ticket model to answer in tg

--- addons/content_support/ticket/show/model.php
+++ addons/content_support/ticket/show/model.php
@@ -4,78 +4,81 @@ namespace content_support\ticket\show;
 class model
 {
 
-	/**
-	 * UploAads an thumb.
-	 *
-	 * @return     boolean  ( description_of_the_return_value )
-	 */
-	public static function upload_file($_name)
-	{
-		if(\dash\request::files($_name))
-		{
-			$uploaded_file = \dash\app\file::upload(['debug' => false, 'upload_name' => $_name]);
-
-			if(isset($uploaded_file['url']))
-			{
-				return $uploaded_file['url'];
-			}
-			// if in upload have error return
-			if(!\dash\engine\process::status())
-			{
-				return false;
-			}
-		}
-		return null;
-	}
-
-
 	public static function post()
 	{
-		$file     = self::upload_file('file');
 
-		// we have an error in upload file1
-		if($file === false)
-		{
-			return false;
-		}
+		$id = \dash\request::get('id');
 
 		if(\dash\request::post('TicketFormType') === 'tag')
 		{
-			\dash\permission::access('supportTicketAssignTag');
-
-			if(!\dash\permission::check('cpTagSupportAdd'))
+			if(self::save_tag($id, \dash\request::post('tag')))
 			{
-				$current_tag = \dash\db\terms::get(['type' => 'support_tag']);
-				if(is_array($current_tag))
+				\dash\redirect::pwd();
+			}
+			return false;
+		}
+
+		if(\dash\request::post('TicketFormType') === 'changeStatus')
+		{
+			if(self::change_status($id, \dash\request::post('status')))
+			{
+				\dash\redirect::pwd();
+			}
+			return false;
+		}
+
+		if(\dash\request::post('TicketFormSolved') === 'solvedForm')
+		{
+			if(self::save_solved($id, \dash\request::post('solved')))
+			{
+				\dash\redirect::pwd();
+			}
+			return false;
+		}
+
+		if(self::answer_save($id, \dash\request::post('content') ? $_POST['content'] : null, \dash\request::post('addnote')))
+		{
+			\dash\redirect::pwd();
+		}
+
+	}
+
+
+	public static function save_tag($_id, $_tag)
+	{
+		\dash\permission::access('supportTicketAssignTag');
+
+		if(!\dash\permission::check('cpTagSupportAdd'))
+		{
+			$current_tag = \dash\db\terms::get(['type' => 'support_tag']);
+			if(is_array($current_tag))
+			{
+				$tag_titles = array_column($current_tag, 'title');
+				$new_tag    = $_tag;
+				$new_tag    = explode(',', $new_tag);
+				foreach ($new_tag as $key => $value)
 				{
-					$tag_titles = array_column($current_tag, 'title');
-					$new_tag    = \dash\request::post('tag');
-					$new_tag    = explode(',', $new_tag);
-					foreach ($new_tag as $key => $value)
+					if(!in_array($value, $tag_titles))
 					{
-						if(!in_array($value, $tag_titles))
-						{
-							\dash\notif::error(T_("Please select tag from list"), 'tag');
-							return false;
-						}
+						\dash\notif::error(T_("Please select tag from list"), 'tag');
+						return false;
 					}
 				}
 			}
-
-			\dash\app::variable(['support_tag' => \dash\request::post('tag')]);
-			\dash\app\posts::set_post_term(\dash\request::get('id'), 'support_tag', 'comments');
-			\dash\log::set('ticketAddTag', ['code' => \dash\request::get('id'), 'tag' => \dash\request::post('tag')]);
-			\dash\notif::ok(T_("Tag was saved"));
-
-			if(!\dash\request::post('content'))
-			{
-				\dash\redirect::pwd();
-				return true;
-			}
 		}
 
+		\dash\app::variable(['support_tag' => $_tag]);
+		\dash\app\posts::set_post_term($_id, 'support_tag', 'comments');
+		\dash\log::set('ticketAddTag', ['code' => $_id, 'tag' => $_tag]);
+		\dash\notif::ok(T_("Tag was saved"));
+		return true;
+	}
 
-		$main = \dash\app\ticket::get(\dash\request::get('id'));
+
+
+	public static function change_status($_id, $_status)
+	{
+		$main = \dash\app\ticket::get($_id);
 
 		if(!$main || !array_key_exists('user_id', $main))
 		{
@@ -85,103 +88,132 @@ class model
 		// check is my ticket and some permission to load guest , ...
 		$is_my_ticket = \content_support\ticket\show\view::is_my_ticket($main);
 
+		$status = $_status;
 
-		if(\dash\request::post('TicketFormType') === 'changeStatus')
+		if(!in_array($status, ['close','deleted','awaiting']))
 		{
-			$status = \dash\request::post('status');
+			\dash\notif::error(T_("Invalid status"));
+			return false;
+		}
 
-			if(!in_array($status, ['close','deleted','awaiting']))
-			{
-				\dash\notif::error(T_("Invalid status"));
-				return false;
-			}
-
-			if(!$is_my_ticket)
-			{
-				switch ($status)
-				{
-					case 'close':
-						$log =
-						[
-							'code' => \dash\request::get('id'),
-
-						];
-						\dash\log::set('setCloseTicket', $log);
-
-						\dash\permission::access('supportTicketClose');
-						break;
-
-					case 'awaiting':
-						\dash\log::set('setAwaitingTicket', ['code' => \dash\request::get('id')]);
-						\dash\permission::access('supportTicketReOpen');
-						break;
-
-					case 'deleted':
-						\dash\log::set('setDeleteTicket', ['code' => \dash\request::get('id')]);
-						\dash\permission::access('supportTicketDelete');
-						break;
-
-				}
-			}
-
-			\dash\db\comments::update(['status' => $status], \dash\request::get('id'));
-
+		if(!$is_my_ticket)
+		{
 			switch ($status)
 			{
 				case 'close':
-					\dash\notif::warn(T_("Ticket closed"));
+					$log =
+					[
+						'code' => $_id,
+
+					];
+					\dash\log::set('setCloseTicket', $log);
+
+					\dash\permission::access('supportTicketClose');
 					break;
 
 				case 'awaiting':
-					\dash\notif::ok(T_("Ticket was open again"));
+					\dash\log::set('setAwaitingTicket', ['code' => $_id]);
+					\dash\permission::access('supportTicketReOpen');
 					break;
 
 				case 'deleted':
-					\dash\notif::warn(T_("Ticket was deleted"));
+					\dash\log::set('setDeleteTicket', ['code' => $_id]);
+					\dash\permission::access('supportTicketDelete');
 					break;
+
 			}
-
-			\dash\redirect::pwd();
-
-			return true;
 		}
 
+		\dash\db\comments::update(['status' => $status], $_id);
 
-		if(\dash\request::post('TicketFormSolved') === 'solvedForm')
+		switch ($status)
 		{
-			\dash\permission::access('supportTicketAnswer');
+			case 'close':
+				\dash\notif::warn(T_("Ticket closed"));
+				break;
 
-			$solved = \dash\request::post('solved') ? 1 : null;
+			case 'awaiting':
+				\dash\notif::ok(T_("Ticket was open again"));
+				break;
 
-			\dash\db\comments::update(['solved' => $solved], \dash\request::get('id'));
-			if($solved)
-			{
-				\dash\log::set('setSolvedTicket', ['code' => \dash\request::get('id')]);
-				\dash\notif::ok(T_("Ticket set as solved"));
-			}
-			else
-			{
-				\dash\log::set('setUnSolvedTicket', ['code' => \dash\request::get('id')]);
-				\dash\notif::warn(T_("Ticket set as unsolved"));
-			}
-			\dash\redirect::pwd();
-
-			return true;
+			case 'deleted':
+				\dash\notif::warn(T_("Ticket was deleted"));
+				break;
 		}
+
+		return true;
+	}
+
+
+	public static function save_solved($_id, $_solved)
+	{
+
+		\dash\permission::access('supportTicketAnswer');
+
+		$solved = $_solved ? 1 : null;
+
+		\dash\db\comments::update(['solved' => $solved], $_id);
+		if($solved)
+		{
+			\dash\log::set('setSolvedTicket', ['code' => $_id]);
+			\dash\notif::ok(T_("Ticket set as solved"));
+		}
+		else
+		{
+			\dash\log::set('setUnSolvedTicket', ['code' => $_id]);
+			\dash\notif::warn(T_("Ticket set as unsolved"));
+		}
+
+		return true;
+
+	}
+
+	public static function answer_save($_id, $_answer, $_type = 'ticket')
+	{
+		$file     = self::upload_file('file');
+
+		// we have an error in upload file1
+		if($file === false)
+		{
+			return false;
+		}
+
+		$main = \dash\app\ticket::get($_id);
+
+		if(!$main || !array_key_exists('user_id', $main))
+		{
+			\dash\header::status(403, T_("Ticket not found"));
+		}
+
+		// check is my ticket and some permission to load guest , ...
+		$is_my_ticket = \content_support\ticket\show\view::is_my_ticket($main);
 
 		$msg      = T_("Your ticket was sended");
 		$notif_fn = 'ok';
 
-		$plus = \dash\db\comments::get_count(['type' => 'ticket', 'parent' => \dash\request::get('id')]);
+
+		$content = $_answer;
+
+		if(\dash\permission::check('supportTicketSignature'))
+		{
+
+			if((mb_strlen($content) - 1) === (mb_strlen(\dash\user::detail('signature'))))
+			{
+				$content = null;
+			}
+		}
+
+		$plus = \dash\db\comments::get_count(['type' => 'ticket', 'parent' => $_id]);
 
 		$ticket_type = 'ticket';
+
 		if(\dash\permission::check('supportTicketAddNote'))
 		{
-			if(\dash\request::post('addnote') === 'note')
+			if($_type === 'note')
 			{
 				$log =
 				[
-					'code'     => \dash\request::get('id'),
+					'code'     => $_id,
 					'tcontent' => $content,
 					'file'     => $file ? $file :"\n",
 					'plus'     => $plus,
@@ -189,25 +221,12 @@ class model
 
 				\dash\log::set('AddNoteTicket', $log);
 
-				$msg = T_("Your note saved");
-				$notif_fn = 'warn';
+				$msg         = T_("Your note saved");
+				$notif_fn    = 'warn';
 				$ticket_type = 'ticket_note';
 			}
 		}
 
-		if(\dash\permission::check('supportTicketSignature'))
-		{
-			$content = \dash\request::post('content') ? $_POST['content'] : null;
-
-			if((mb_strlen($content) - 1) === (mb_strlen(\dash\user::detail('signature'))))
-			{
-				$content = null;
-			}
-		}
-		else
-		{
-			$content = \dash\request::post('content');
-		}
 
 		// ready to insert comments
 		$args =
@@ -219,7 +238,7 @@ class model
 			// 'title'   => \dash\request::post('title'),
 			'mobile'  => \dash\user::detail("mobile"),
 			'user_id' => \dash\user::id(),
-			'parent'  => \dash\request::get('id'),
+			'parent'  => $_id,
 			'file'    => $file,
 		];
 
@@ -244,7 +263,7 @@ class model
 
 					$log =
 					[
-						'code'     => \dash\request::get('id'),
+						'code'     => $_id,
 						'tcontent' => $content,
 						'file'     => $file ? $file :"\n",
 						'plus'     => $update_main['plus'],
@@ -267,7 +286,7 @@ class model
 
 					$log =
 					[
-						'code'     => \dash\request::get('id'),
+						'code'     => $_id,
 						'tcontent' => $content,
 						'file'     => $file ? $file :"\n",
 						'plus'     => $update_main['plus'],
@@ -300,12 +319,40 @@ class model
 		{
 			if(!empty($update_main))
 			{
-				\dash\db\comments::update($update_main, \dash\request::get('id'));
+				\dash\db\comments::update($update_main, $_id);
 			}
 
 			\dash\notif::$notif_fn($msg);
-			\dash\redirect::pwd();
+			return true;
 		}
+		return false;
 	}
+
+
+
+	/**
+	 * UploAads an thumb.
+	 *
+	 * @return     boolean  ( description_of_the_return_value )
+	 */
+	public static function upload_file($_name)
+	{
+		if(\dash\request::files($_name))
+		{
+			$uploaded_file = \dash\app\file::upload(['debug' => false, 'upload_name' => $_name]);
+
+			if(isset($uploaded_file['url']))
+			{
+				return $uploaded_file['url'];
+			}
+			// if in upload have error return
+			if(!\dash\engine\process::status())
+			{
+				return false;
+			}
+		}
+		return null;
+	}
+
 }
 ?>
