e4808d24962ff7a357035fd2b391b82fdbb1414f
Reza <rm.biqarar@gmail.com>
2018-10-02 09:42:21 +0330

    show active session in account

--- addons/content_account/profile/security/model.php
+++ addons/content_account/profile/security/model.php
@@ -57,6 +57,17 @@ class model
 	 */
 	public static function post()
 	{
+		if(\dash\request::post('type') === 'terminate' && \dash\request::post('id') && is_numeric(\dash\request::post('id')))
+		{
+			if(\dash\db\sessions::is_my_session(\dash\request::post('id'), \dash\user::id()))
+			{
+				\dash\log::set('sessionTerminate');
+				\dash\db\sessions::terminate_id(\dash\request::post('id'));
+				\dash\notif::ok(T_("Session terminated"));
+				\dash\redirect::pwd();
+				return true;
+			}
+		}
 
 		$request = self::getPost();
 
