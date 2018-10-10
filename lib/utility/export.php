e893f397cd5c54cf5f786f75288bf6175b72ae7b
Reza <rm.biqarar@gmail.com>
2018-10-10 21:09:24 +0330

    add big data export

--- lib/utility/export.php
+++ lib/utility/export.php
@@ -69,5 +69,75 @@ class export
         \dash\code::bye();
     }
 
+    private static $temp_file = null;
+
+
+    public static function get_link()
+    {
+        $url = self::$temp_file;
+        $url = str_replace(root.'public_html', \dash\url::site(), $url);
+        return $url;
+    }
+
+
+    public static function csv_file($_args)
+    {
+
+        $type     = isset($_args['type']) ? $_args['type'] : 'csv';
+        $filename = isset($_args['name']) ? $_args['name'] : 'Untitled';
+
+        if(!self::$temp_file)
+        {
+            self::$temp_file = root. 'public_html/files';
+            if(!\dash\file::exists(self::$temp_file))
+            {
+                \dash\file::makeDir(self::$temp_file);
+            }
+            self::$temp_file .= '/temp';
+            if(!\dash\file::exists(self::$temp_file))
+            {
+                \dash\file::makeDir(self::$temp_file);
+            }
+            self::$temp_file.= '/'. date("Y_m_d_H_i_s"). '_'. rand(1,999).'_'.$filename. '.'. $type;
+
+            // BOM header UTF-8
+            file_put_contents(self::$temp_file, "\xEF\xBB\xBF");
+        }
+
+        $data     = isset($_args['data']) ? $_args['data'] : [];
+        $ignore   = isset($_args['ignore']) ? $_args['ignore'] : [];
+
+        if (count($data) == 0 || !$data || empty($data) || !is_array($data))
+        {
+            return;
+        }
+        else
+        {
+            ob_start();
+            $df = @fopen("php://output", 'w');
+            if(is_array(reset($data)))
+            {
+                $keys = array_keys(reset($data));
+                $keys = array_map('T_', $keys);
+
+                fputcsv($df, $keys);
+            }
+
+            foreach ($data as $row)
+            {
+                if(!is_array($row))
+                {
+                    $row = [$row];
+                }
+                fputcsv($df, $row);
+            }
+
+            fclose($df);
+            file_put_contents(self::$temp_file, ob_get_clean(), FILE_APPEND);
+        }
+
+        return true;
+    }
+
 }
 ?>
