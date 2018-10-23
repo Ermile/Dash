cc0eeabd3aaf46a442f760d2f0f5153948c4a667
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-23 18:25:01 +0330

    now we are not returning result

--- addons/content_hook/tg/controller.php
+++ addons/content_hook/tg/controller.php
@@ -19,21 +19,6 @@ class controller
 
 			// fire telegram api
 			$result = \dash\social\telegram\tg::fire();
-			if(isset($_SERVER['HTTP_POSTMAN_TOKEN']))
-			{
-				if($result)
-				{
-					echo("This result only show in postman!)\n");
-					$result = \dash\social\telegram\log::json($result);
-					echo($result);
-					echo("\n--debug\n");
-				}
-				else
-				{
-					echo("Final result is empty!");
-				}
-
-			}
 
 			// bobooom :)
 			\dash\code::boom();
