2d4e2dbef9d92fb03b7dd53d48b06da21b8a726b
Reza <rm.biqarar@gmail.com>
2018-10-23 18:35:44 +0330

    test add new ticket

--- lib/app/tg/ticket.php
+++ lib/app/tg/ticket.php
@@ -14,7 +14,23 @@ class ticket
 
 	public static function create($_title, $_content)
 	{
-		\content_support\ticket\add\model::add_new($_title, $_content);
+		// ready to insert comments
+		$args =
+		[
+			'author'  => \dash\user::detail('displayname'),
+			'email'   => \dash\user::detail('email'),
+			'type'    => 'ticket',
+			'content' => $_content,
+			'title'   => $_title,
+			'mobile'  => \dash\user::detail("mobile"),
+			'file'    => $_file,
+			'user_id' => \dash\user::id(),
+		];
+
+		// insert comments
+		$result = \dash\app\ticket::add($args);
+
+		// \content_support\ticket\add\model::add_new($_title, $_content);
 	}
 }
 ?>
