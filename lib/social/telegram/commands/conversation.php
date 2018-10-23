5637c78318e78d06d3fa3b14cd24a164ed6d2f1f
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-23 16:50:03 +0330

    clean conversation and use utility

--- lib/social/telegram/commands/conversation.php
+++ lib/social/telegram/commands/conversation.php
@@ -48,55 +48,6 @@ class conversation
 				$text = T_('Test <b>:name</b> bot on :site', ['name' => bot::$name, 'site' => \dash\url::kingdom()]);
 				break;
 
-			case '/userid':
-			case 'userid':
-			case '/user_id':
-			case 'user_id':
-			case '/myid':
-			case 'myid':
-				$text = T_("User id"). ' '. \dash\user::id();
-				$text .= "\n\n<pre>". json_encode(\dash\social\telegram\hook::from(null), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE). "</pre>";
-				break;
-
-			case '/session':
-			case 'session':
-				$chatID = \dash\social\telegram\hook::from();
-				if($chatID === 46898544 || $chatID === 344542267 || $chatID === 33263188)
-				{
-					// temporary send tg result
-					$_SESSION['tg'][date('Y-m-d H:i:s')] = '🔸 '. \dash\user::id();
-					$text      = "\n\n<pre>". json_encode($_SESSION, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)."</pre>";
-				}
-				else
-				{
-					$text = "Hello my son:)";
-				}
-				break;
-
-
-			case '/logout':
-			case 'logout':
-					bot::sendMessage('📴 '.T_("Booom"));
-					\dash\user::destroy();
-					\dash\code::boom();
-				break;
-
-
-			case '/tgsession':
-			case 'tgsession':
-				$chatID = \dash\social\telegram\hook::from();
-				if($chatID === 46898544 || $chatID === 344542267 || $chatID === 33263188)
-				{
-					// temporary send tg result
-					$_SESSION['tg'][date('Y-m-d H:i:s')] = '🔸 '. \dash\user::id();
-					$text      = "\n\n<pre>". json_encode($_SESSION['tg'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)."</pre>";
-				}
-				else
-				{
-					$text = "Hi tg baby:)";
-				}
-				break;
-
 
 			default:
 				$text = false;
