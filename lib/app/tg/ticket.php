b79f7841197f574594b08a568ade64b4a58d493a
Reza <rm.biqarar@gmail.com>
2018-10-28 10:46:34 +0330

    clean tg msg

--- lib/app/tg/ticket.php
+++ lib/app/tg/ticket.php
@@ -14,21 +14,6 @@ class ticket
 
 	public static function create($_content)
 	{
-		// $args =
-		// [
-		// 	'author'  => \dash\user::detail('displayname'),
-		// 	'email'   => \dash\user::detail('email'),
-		// 	'type'    => 'ticket',
-		// 	'content' => $_content,
-		// 	'title'   => T_("Ticket via telegram"),
-		// 	'mobile'  => \dash\user::detail("mobile"),
-		// 	'file'    => null,
-		// 	'user_id' => \dash\user::id(),
-		// ];
-
-		// // insert comments
-		// $result = \dash\app\ticket::add($args);
-
 		\content_support\ticket\add\model::add_new(T_("Ticket via telegram"), $_content);
 	}
 
@@ -48,30 +33,11 @@ class ticket
 
 		$msg = '';
 		$msg .= "🆔#Ticket".$_id. "\n";
-		// $msg .= " #New \n🗣 ". \dash\data::masterTicketDetail_displayname(). " #user". \dash\data::masterTicketDetail_user_id();
-		// $msg .= "\n—————\n📬 ";
-
-		// if(isset($masterTicketDetail['title']))
-		// {
-		// 	$msg .= strip_tags($masterTicketDetail['title']). "\n";
-		// }
-
-		// if(isset($masterTicketDetail['content']))
-		// {
-		// 	$msg .= strip_tags($masterTicketDetail['content']). "\n";
-		// }
-
-		// if(isset($masterTicketDetail['datecreated']))
-		// {
-		// 	$msg .= "\n⏳ ". \dash\datetime::fit($masterTicketDetail['datecreated'], true);
-		// }
 
 		if(is_array($dataTable))
 		{
 			foreach ($dataTable as $key => $value)
 			{
-				// $key_fit = \dash\utility\human::fitNumber($key + 1);
-				// $msg .= "🔄 $key_fit\n"
 				$msg .= "🗣 ". @$value['displayname']. " #user". @$value['user_id'];
 
 				if(isset($value['title']))
