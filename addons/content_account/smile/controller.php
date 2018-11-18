49029df4e46f67568b818ee4060589ebce577b69
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-18 18:27:40 +0330

    change sample to new one

--- addons/content_account/smile/controller.php
+++ addons/content_account/smile/controller.php
@@ -7,28 +7,38 @@ class controller
 
 	public static function routing()
 	{
-		if(!\dash\user::id())
-		{
-			// logout sample
-			$result =
-			[
-				'okay'      => false,
-				'logoutTxt' => T_("Goodbye"),
-				'logoutUrl' => \dash\url::kingdom(). '/logout'
-				// 'logoutUrl' => \dash\url::kingdom(). '/logout?mobile='. \dash\user::mobile()
-			];
-		}
-		else
-		{
-			$result =
-			[
-				'okay'     => true,
-				'newNotif' => (bool)random_int(0, 1),
-			];
-		}
+		// if(!\dash\user::id())
+		// {
+		// 	// logout sample
+		// 	$result =
+		// 	[
+		// 		'okay'      => false,
+		// 		'logoutTxt' => T_("Goodbye"),
+		// 		'logoutUrl' => \dash\url::kingdom(). '/logout'
+		// 		// 'logoutUrl' => \dash\url::kingdom(). '/logout?mobile='. \dash\user::mobile()
+		// 	];
+		// }
+		// else
+		// {
+		// 	$result =
+		// 	[
+		// 		'okay'     => true,
+		// 		'newNotif' => (bool)random_int(0, 1),
+		// 	];
+		// }
 
+		$myResult =
+		[
+			'okay'     => true,
+			'newNotif' => (bool)random_int(0, 1),
+		];
 
-		\dash\code::jsonBoom($result);
+		\dash\notif::info(T_("Salam"));
+
+		\dash\notif::result($myResult);
+		$notifResult = \dash\notif::get();
+
+		\dash\code::jsonBoom($notifResult);
 	}
 }
 ?>
