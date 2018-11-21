2aab6959e662fc8eb938d66994c88ea1396e3fb0
Reza <rm.biqarar@gmail.com>
2018-11-21 10:09:35 +0330

    fix alert and alert send notif

--- lib/app/log/caller/ticket/ticket_answerTicketAlert.php
+++ lib/app/log/caller/ticket/ticket_answerTicketAlert.php
@@ -7,8 +7,24 @@ class ticket_answerTicketAlert
 	public static function site($_args = [])
 	{
 		$code = isset($_args['code']) ? $_args['code'] : null;
-		$title    = T_("Regards"). "\n". T_("Ticket :val answered", ['val' => \dash\utility\human::fitNumber($code, false)]);
-		return $title;
+
+		$result              = [];
+		$result['title']     = T_("Regards"). "\n". T_("Ticket :val answered", ['val' => \dash\utility\human::fitNumber($code, false)]);;
+		$result['icon']      = 'life-ring';
+		$result['cat']       = T_("Support");
+		$result['iconClass'] = 'fc-red';
+
+
+		$excerpt = '';
+		$excerpt .=	'<a href="'.\dash\url::kingdom(). '/!'. $code. '">';
+		$excerpt .= T_("Show ticket");
+		$excerpt .= ' ';
+		$excerpt .= \dash\utility\human::fitNumber($code, false);
+		$excerpt .= '</a>';
+
+		$result['txt'] = $excerpt;
+
+		return $result;
 	}
 
 
