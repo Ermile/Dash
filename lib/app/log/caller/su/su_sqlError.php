8d1fb0bb448b783d07957e378b190ebaa064f6e2
Reza <rm.biqarar@gmail.com>
2018-11-23 10:13:43 +0330

    save caller of php bug and sql bug to save it

---
+++ lib/app/log/caller/su/su_sqlError.php
@@ -0,0 +1,67 @@
+<?php
+namespace dash\app\log\caller\su;
+
+class su_sqlError
+{
+	public static function site($_args = [])
+	{
+
+		$excerpt = T_("Database have error!!");
+		$mymsg = isset($_args['mymsg']) ? $_args['mymsg'] : null;
+
+		$result              = [];
+		$result['title']     = T_("Sql error!");
+		$result['icon']      = 'bug-2';
+		$result['cat']       = T_("Bug");
+		$result['iconClass'] = 'fc-red';
+		$result['excerpt']   = $excerpt;
+		$result['txt']   = '<code>'. $mymsg. '</code>';
+		return $result;
+	}
+
+
+	public static function send_to()
+	{
+		return ['supervisor'];
+	}
+
+	public static function is_notif()
+	{
+		return true;
+	}
+
+	public static function telegram()
+	{
+		return true;
+	}
+
+	public static function expire()
+	{
+		return date("Y-m-d H:i:s", strtotime("+30 days"));
+	}
+
+
+
+	public static function telegram_text($_args, $_chat_id)
+	{
+		$mymsg = isset($_args['mymsg']) ? $_args['mymsg'] : null;
+
+		$tg_msg = '';
+		$tg_msg .= "🥁#SQLERROR\n";
+		$tg_msg .= '<code>';
+		$tg_msg .= $mymsg;
+		$tg_msg .= '</code>';
+
+		$tg_msg .= "\n⏳ ". \dash\datetime::fit(date("Y-m-d H:i:s"), true);
+
+		$tg                 = [];
+		$tg['chat_id']      = $_chat_id;
+		$tg['text']         = $tg_msg;
+		$tg['reply_markup'] = false;
+
+		$tg = json_encode($tg, JSON_UNESCAPED_UNICODE);
+
+		return $tg;
+	}
+}
+?>
