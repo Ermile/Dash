19e3f9614ec37925892ba35899b36ebf52a911f0
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-29 16:38:58 +0330

    improve desing of log

--- lib/engine/version.php
+++ lib/engine/version.php
@@ -6,7 +6,7 @@ namespace dash\engine;
 class version
 {
 	// @var dash engine current version
-	const version = '18.1.0';
+	const version = '18.2.0';
 
 	// @var dash engine current commit number
 	// now get it automatically from git commands
