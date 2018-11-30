43f811faabe6f056a4d410f42cc7578c374ab75a
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-30 19:17:30 +0330

    add langlist fn and use in inside profile alerty

--- lib/engine/view.php
+++ lib/engine/view.php
@@ -274,16 +274,21 @@ class view
 
 			$txtFooter = '';
 			$txtFooter .= '<div class="align-center">';
+			$txtFooter .= ' <div>';
 			// notification
-			$txtFooter .= ' <a class="btn outline lg" href="'. \dash\url::kingdom(). '/account/notification" title="'. T_("Notifications"). '">'. '<i class="sf-bell"></i>'. '</a>';
+			$txtFooter .= '  <a class="btn outline lg" href="'. \dash\url::kingdom(). '/account/notification" title="'. T_("Notifications"). '">'. '<i class="sf-bell"></i>'. '</a>';
 			// profile
-			$txtFooter .= ' <a class="btn outline lg" href="'. \dash\url::kingdom(). '/account/profile" title="'. T_("Profile"). '">'. '<i class="sf-user"></i>'. '</a>';
+			$txtFooter .= '  <a class="btn outline lg" href="'. \dash\url::kingdom(). '/account/profile" title="'. T_("Profile"). '">'. '<i class="sf-user"></i>'. '</a>';
 			// billing
-			$txtFooter .= ' <a class="btn outline lg" href="'. \dash\url::kingdom(). '/account/billing" title="'. T_("Billing"). '">'. '<i class="sf-credit-card"></i>'. '</a>';
+			$txtFooter .= '  <a class="btn outline lg" href="'. \dash\url::kingdom(). '/account/billing" title="'. T_("Billing"). '">'. '<i class="sf-credit-card"></i>'. '</a>';
 			// support
-			$txtFooter .= ' <a class="btn outline lg" href="'. \dash\url::kingdom(). '/support" title="'. T_("Support center"). '">'. '<i class="sf-life-ring"></i>'. '</a>';
+			$txtFooter .= '  <a class="btn outline lg" href="'. \dash\url::kingdom(). '/support" title="'. T_("Support center"). '">'. '<i class="sf-life-ring"></i>'. '</a>';
 			// ticket
-			$txtFooter .= ' <a class="btn outline lg" href="'. \dash\url::kingdom(). '/support/ticket" title="'. T_("Tickets"). '">'. '<i class="sf-question-circle"></i>'. '</a>';
+			$txtFooter .= '  <a class="btn outline lg" href="'. \dash\url::kingdom(). '/support/ticket" title="'. T_("Tickets"). '">'. '<i class="sf-question-circle"></i>'. '</a>';
+			$txtFooter .= ' </div>';
+
+			// add language bar
+			$txtFooter .= \dash\language::langList(['html' => true, 'all' => true, 'class' => 'f']);
 			$txtFooter .= '</div>';
 
 			\dash\data::userBadge_desc($txtDesc);
