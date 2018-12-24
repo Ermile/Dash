0cdc9607d834247b44586846ec361edb406a1f8e
Reza <rm.biqarar@gmail.com>
2018-12-24 08:47:51 +0330

    fix call db transaction function

--- lib/utility/payment/transactions.php
+++ lib/utility/payment/transactions.php
@@ -60,8 +60,22 @@ class transactions
 
 	public static function final_verify($_transaction_id)
 	{
-		$fn = self::transaction_table_name('final_verify');
-		return $fn::final_verify($_transaction_id);
+		$project_fn = ["\\lib\\db\\transactions", "final_verify"];
+		$dash_fn    = ["\\dash\\db\\transactions", "final_verify"];
+
+		if(is_callable($project_fn))
+		{
+			$namespace = $project_fn[0];
+			$function  = $project_fn[1];
+			return $namespace::$function($_transaction_id);
+		}
+		else
+		{
+			$namespace = $dash_fn[0];
+			$function  = $dash_fn[1];
+			return $namespace::$function($_transaction_id);
+		}
+
 	}
 }
 ?>
