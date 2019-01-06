fa8a0b75f424146ac40d4c168326fa49c51ca5ca
Reza <rm.biqarar@gmail.com>
2019-01-06 13:46:33 +0330

    fix upgrade db error in old version

--- addons/includes/database/upgrade/(db_name)/v.2.0.8_user_tg_status_fix2.sql
+++ addons/includes/database/upgrade/(db_name)/v.2.0.8_user_tg_status_fix2.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `users` ADD `tgstatus` ENUM('active','deactive','spam','bot','block','unreachable','unknown','filter') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `users` CHANGE `tgstatus` `tgstatus` ENUM('active','deactive','spam','bot','block','unreachable','unknown','filter', 'awaiting') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
--- addons/includes/database/upgrade/(db_name)/v.2.5.7_fix_send_2.sql
+++
@@ -1,6 +0,0 @@
--- [database log]
-
-ALTER TABLE `telegrams` ADD `send2` mediumtext CHARACTER SET utf8mb4;
-ALTER TABLE `telegrams` ADD `response2` mediumtext CHARACTER SET utf8mb4;
-ALTER TABLE `telegrams` ADD `send3` mediumtext CHARACTER SET utf8mb4;
-ALTER TABLE `telegrams` ADD `response3` mediumtext CHARACTER SET utf8mb4;
