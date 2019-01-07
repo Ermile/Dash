11955080b985b25504dbe52312943fb31b87fe97
Reza <rm.biqarar@gmail.com>
2019-01-07 14:47:05 +0330

    fix nationalcode check

--- lib/utility/filter.php
+++ lib/utility/filter.php
@@ -29,23 +29,6 @@ class filter
 			return false;
 		}
 
-		$fake =
-		[
-			1111111111,
-			2222222222,
-			3333333333,
-			4444444444,
-			5555555555,
-			6666666666,
-			7777777777,
-			8888888888,
-			9999999999,
-		];
-
-		if(in_array(intval($_national_code), $fake))
-		{
-			return false;
-		}
 
 		$split      = str_split($_national_code);
 		$main_place = [];
