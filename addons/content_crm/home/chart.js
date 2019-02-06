e4080c23fe40de8e8051cf24a66682882191148d
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-06 16:21:37 +0330

    fix chart indents

--- addons/content_crm/home/chart.js
+++ addons/content_crm/home/chart.js
@@ -1,6 +1,6 @@
 function chartDrawer()
 {
-  if($("#chartdiv").length == 1){highChart();}
+  if($("#identifyChart").length == 1){identifyChart();}
   if($("#genderchart").length == 1){gender_chart();}
   if($("#statuschart").length == 1){status_chart();}
   if($("#syncedChart").length == 1){syncedChart();}
@@ -8,101 +8,102 @@ function chartDrawer()
 
 
 
-function highChart()
+function identifyChart()
 {
 
-Highcharts.chart('chartdiv',
-{
-  chart: {
-    zoomType: 'x',
-    style: {
-      fontFamily: 'IRANSans, Tahoma, sans-serif'
-    }
-  },
-  title: {
-    text: '{%trans "Users group by identify"%}'
-  },
-  xAxis: [{
-    categories: {{dashboardDetail.chart.identify.categories | raw}},
-    crosshair: true
-  }],
-  yAxis: [{ // Primary yAxis
-    labels: {
-      format: '{value}',
+  Highcharts.chart('identifyChart',
+  {
+    chart: {
+      zoomType: 'x',
       style: {
-        color: Highcharts.getOptions().colors[0]
+        fontFamily: 'IRANSans, Tahoma, sans-serif'
       }
     },
     title: {
-      text: '{%trans "Person"%}',
-      useHTML: Highcharts.hasBidiBug,
-      style: {
-        color: Highcharts.getOptions().colors[0]
-      }
-    }
-  },
-  { // Secondary yAxis
-    title: {
-      text: '{%trans "Person"%}',
-      useHTML: Highcharts.hasBidiBug,
-      style: {
-        color: Highcharts.getOptions().colors[1]
-      }
+      text: '{%trans "Users group by identify"%}'
     },
-    labels: {
-      format: '{value}',
-      style: {
-        color: Highcharts.getOptions().colors[1]
-      }
-    },
-    opposite: true
-  }],
-  tooltip: {
-    useHTML: true,
-    borderWidth: 0,
-    shared: true
-  },
-  exporting:
-  {
-    enabled: false
-  },
-  credits:
-  {
-      text: '{{service.title}}',
-      href: '{{service.url}}',
-      position:
-      {
-          x: -35,
-          y: -7
+    xAxis: [{
+      categories: {{dashboardDetail.chart.identify.categories | raw}},
+      crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
       },
-      style: {
-          fontWeight: 'bold'
+      title: {
+        text: '{%trans "Person"%}',
+        useHTML: Highcharts.hasBidiBug,
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
       }
-  },
-  legend: {
-    layout: 'vertical',
-    align: 'left',
-    x: 120,
-    verticalAlign: 'top',
-    y: 100,
-    floating: true,
-    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
-  },
-  series: [
-  {
-    name: '{%trans "Count"%}',
-    type: 'column',
-    data: {{dashboardDetail.chart.identify.value | raw}},
+    },
+    { // Secondary yAxis
+      title: {
+        text: '{%trans "Person"%}',
+        useHTML: Highcharts.hasBidiBug,
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      },
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      },
+      opposite: true
+    }],
     tooltip: {
-      valueSuffix: ' {%trans "Person"%}'
-    }
+      useHTML: true,
+      borderWidth: 0,
+      shared: true
+    },
+    exporting:
+    {
+      enabled: false
+    },
+    credits:
+    {
+        text: '{{service.title}}',
+        href: '{{service.url}}',
+        position:
+        {
+            x: -35,
+            y: -7
+        },
+        style: {
+            fontWeight: 'bold'
+        }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      x: 120,
+      verticalAlign: 'top',
+      y: 100,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
+    },
+    series: [
+    {
+      name: '{%trans "Count"%}',
+      type: 'column',
+      data: {{dashboardDetail.chart.identify.value | raw}},
+      tooltip: {
+        valueSuffix: ' {%trans "Person"%}'
+      }
 
-  }
-  ]
-}, function(_chart)
-  {
-    _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
-  });
+    }
+    ]
+  }, function(_chart)
+    {
+      _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
+    }
+  );
 }
 
 
@@ -114,46 +115,48 @@ Highcharts.chart('chartdiv',
 function gender_chart()
 {
 
-Highcharts.chart('genderchart', {
-  chart: {
+  Highcharts.chart('genderchart',
+  {
+    chart: {
 
-    style: {
-      fontFamily: 'IRANSans, Tahoma, sans-serif'
+      style: {
+        fontFamily: 'IRANSans, Tahoma, sans-serif'
+      },
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
     },
-    plotBackgroundColor: null,
-    plotBorderWidth: null,
-    plotShadow: false,
-    type: 'pie'
-  },
-  title: {
-    text: '{%trans "Users group by gender"%}'
-  },
-  tooltip: {
-    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      cursor: 'pointer',
-      dataLabels: {
-        enabled: true,
-        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-        style: {
-          color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+    title: {
+      text: '{%trans "Users group by gender"%}'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          style: {
+            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+          }
         }
       }
+    },
+    series: [{
+      name: '{%trans "Gender"%}',
+      // useHTML: Highcharts.hasBidiBug,
+      colorByPoint: true,
+      data: {{dashboardDetail.chart.gender | raw}}
+    }]
+  }, function(_chart)
+    {
+      _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
     }
-  },
-  series: [{
-    name: '{%trans "Gender"%}',
-    // useHTML: Highcharts.hasBidiBug,
-    colorByPoint: true,
-    data: {{dashboardDetail.chart.gender | raw}}
-  }]
-}, function(_chart)
-  {
-    _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
-  });
+  );
 
 }
 
@@ -234,56 +237,6 @@ function status_chart()
 }
 
 
-function status_chart2()
-{
-
-
-Highcharts.chart('statuschart', {
-  chart: {
-    plotBackgroundColor: null,
-    plotBorderWidth: 0,
-    plotShadow: false
-  },
-  title: {
-    text: '{%trans "Users group by"%}<br>{%trans "Status"%}<br>',
-    align: 'center',
-    verticalAlign: 'middle',
-    y: 40
-  },
-  tooltip: {
-    pointFormat: '{series.name}: {point.percentage:.1f}%'
-  },
-  plotOptions: {
-    pie: {
-      dataLabels: {
-        enabled: true,
-        distance: -50,
-        style: {
-          fontWeight: 'bold',
-          color: 'white'
-        }
-      },
-      startAngle: -90,
-      endAngle: 90,
-      center: ['50%', '75%'],
-      size: '110%'
-    }
-  },
-  series: [{
-    type: 'pie',
-    name: 'Browser share',
-    innerSize: '50%',
-    data: {{dashboardDetail.chart.status | raw}}
-  }]
-}, function(_chart)
-  {
-    _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
-  });
-
-
-
-}
-
 
 
 /**
@@ -312,154 +265,162 @@ function syncExtremes(e) {
 
 function syncedChart()
 {
-/*
-The purpose of this demo is to demonstrate how multiple charts on the same page
-can be linked through DOM and Highcharts events and API methods. It takes a
-standard Highcharts config with a small variation for each data set, and a
-mouse/touch event handler to bind the charts together.
-*/
+  /*
+  The purpose of this demo is to demonstrate how multiple charts on the same page
+  can be linked through DOM and Highcharts events and API methods. It takes a
+  standard Highcharts config with a small variation for each data set, and a
+  mouse/touch event handler to bind the charts together.
+  */
 
 
 
-/**
- * In order to synchronize tooltips and crosshairs, override the
- * built-in events with handlers defined on the parent element.
- */
-['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
-  document.getElementById('syncedChart').addEventListener(
-    eventType,
-    function (e) {
-      var chart,
-        point,
-        i,
-        event;
+  /**
+   * In order to synchronize tooltips and crosshairs, override the
+   * built-in events with handlers defined on the parent element.
+   */
+  ['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType)
+  {
+    document.getElementById('syncedChart').addEventListener(
+      eventType,
+      function (e) {
+        var chart,
+          point,
+          i,
+          event;
 
-      for (i = 0; i < Highcharts.charts.length; i = i + 1) {
-        chart = Highcharts.charts[i];
-        // Find coordinates within the chart
-        event = chart.pointer.normalize(e);
-        // Get the hovered point
-        point = chart.series[0].searchPoint(event, true);
+        for (i = 0; i < Highcharts.charts.length; i = i + 1) {
+          chart = Highcharts.charts[i];
+          // Find coordinates within the chart
+          event = chart.pointer.normalize(e);
+          // Get the hovered point
+          point = chart.series[0].searchPoint(event, true);
 
-        if (point) {
-          point.highlight(e);
+          if (point) {
+            point.highlight(e);
+          }
         }
       }
-    }
-  );
-});
+    );
+  });
 
-/**
- * Override the reset function, we don't need to hide the tooltips and
- * crosshairs.
- */
-Highcharts.Pointer.prototype.reset = function () {
-  return undefined;
-};
+  /**
+   * Override the reset function, we don't need to hide the tooltips and
+   * crosshairs.
+   */
+  Highcharts.Pointer.prototype.reset = function ()
+  {
+    return undefined;
+  };
 
-/**
- * Highlight a point by showing tooltip, setting hover state and draw crosshair
- */
-Highcharts.Point.prototype.highlight = function (event) {
-  event = this.series.chart.pointer.normalize(event);
-  this.onMouseOver(); // Show the hover marker
-  this.series.chart.tooltip.refresh(this); // Show the tooltip
-  this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
-};
+  /**
+   * Highlight a point by showing tooltip, setting hover state and draw crosshair
+   */
+  Highcharts.Point.prototype.highlight = function (event)
+  {
+    event = this.series.chart.pointer.normalize(event);
+    this.onMouseOver(); // Show the hover marker
+    this.series.chart.tooltip.refresh(this); // Show the tooltip
+    this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
+  };
 
 
 
-// Get the data. The contents of the data file can be viewed at
-Highcharts.ajax({
-  url: '{{url.kingdom}}/support/ticket/report?ajaxreport=json',
-  dataType: 'text',
-  success: function (activity) {
+  // Get the data. The contents of the data file can be viewed at
+  Highcharts.ajax(
+  {
+    url: '{{url.kingdom}}/support/ticket/report?ajaxreport=json',
+    dataType: 'text',
+    success: function (activity)
+    {
 
-    activity = JSON.parse(activity);
-    activity.datasets.forEach(function (dataset, i) {
+      activity = JSON.parse(activity);
+      activity.datasets.forEach(function (dataset, i)
+      {
 
-      // Add X values
-      dataset.data = Highcharts.map(dataset.data, function (val, j) {
-        return [activity.xData[j], val];
-      });
+        // Add X values
+        dataset.data = Highcharts.map(dataset.data, function (val, j)
+        {
+          return [activity.xData[j], val];
+        });
 
-      var syncedChart = document.createElement('div');
-      syncedChart.className = 'chart';
-      document.getElementById('syncedChart').appendChild(syncedChart);
+        var syncedChart = document.createElement('div');
+        syncedChart.className = 'chart';
+        document.getElementById('syncedChart').appendChild(syncedChart);
 
-      Highcharts.chart(syncedChart, {
-        chart: {
-          marginLeft: 40, // Keep all charts left aligned
-          spacingTop: 20,
-          spacingBottom: 20,
-          height: 220,
-          zoomType: 'x',
-          style: {
-            fontFamily: 'IRANSans, Tahoma, sans-serif'
-          }
-        },
-        title: {
-          text: dataset.name,
-          align: 'left',
-          margin: 0,
-          x: 30
-        },
-        credits: {
-          enabled: false
-        },
-        exporting: {
-          enabled: false
-        },
-        legend: {
-          enabled: false
-        },
-        xAxis: {
-          crosshair: true,
-          events: {
-            setExtremes: syncExtremes
+        Highcharts.chart(syncedChart,
+        {
+          chart: {
+            marginLeft: 40, // Keep all charts left aligned
+            spacingTop: 20,
+            spacingBottom: 20,
+            height: 220,
+            zoomType: 'x',
+            style: {
+              fontFamily: 'IRANSans, Tahoma, sans-serif'
+            }
           },
-          labels: {
-            format: '{value}'
-          }
-        },
-        yAxis: {
-          allowDecimals: false,
           title: {
-            text: null
-          }
-        },
-        tooltip: {
-          positioner: function () {
-            return {
-              // right aligned
-              x: this.chart.chartWidth - this.label.width,
-              y: 10 // align to title
-            };
+            text: dataset.name,
+            align: 'left',
+            margin: 0,
+            x: 30
           },
-          borderWidth: 0,
-          backgroundColor: 'none',
-          pointFormat: '{point.y}',
-          headerFormat: '',
-          shadow: false,
-          style: {
-            fontSize: '18px'
+          credits: {
+            enabled: false
           },
-          valueDecimals: dataset.valueDecimals
-        },
-        series: [{
-          data: dataset.data,
-          name: dataset.name,
-          type: dataset.type,
-          color: Highcharts.getOptions().colors[i],
-          fillOpacity: 0.3,
+          exporting: {
+            enabled: false
+          },
+          legend: {
+            enabled: false
+          },
+          xAxis: {
+            crosshair: true,
+            events: {
+              setExtremes: syncExtremes
+            },
+            labels: {
+              format: '{value}'
+            }
+          },
+          yAxis: {
+            allowDecimals: false,
+            title: {
+              text: null
+            }
+          },
           tooltip: {
-            valueSuffix: ' ' + dataset.unit
-          }
-        }]
+            positioner: function () {
+              return {
+                // right aligned
+                x: this.chart.chartWidth - this.label.width,
+                y: 10 // align to title
+              };
+            },
+            borderWidth: 0,
+            backgroundColor: 'none',
+            pointFormat: '{point.y}',
+            headerFormat: '',
+            shadow: false,
+            style: {
+              fontSize: '18px'
+            },
+            valueDecimals: dataset.valueDecimals
+          },
+          series: [{
+            data: dataset.data,
+            name: dataset.name,
+            type: dataset.type,
+            color: Highcharts.getOptions().colors[i],
+            fillOpacity: 0.3,
+            tooltip: {
+              valueSuffix: ' ' + dataset.unit
+            }
+          }]
+        });
       });
-    });
-  }
-});
+    }
+  });
 
 }
 
