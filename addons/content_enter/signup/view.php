afeba5d6db67bab4ddbb6ec9441b9fea56f764b3
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-09 21:27:47 +0330

    add agreement into enter process

--- addons/content_enter/signup/view.php
+++ addons/content_enter/signup/view.php
@@ -12,6 +12,12 @@ class view
 
 		// set el value to use in display
 		\dash\data::el_username(\dash\option::config('enter', 'singup_username'));
+
+
+		$termLink = '<a href="'. \dash\url::kingdom(). '/terms" target="_blank">'. T_('Terms of Service') .'</a>';
+		$privacyLink = '<a href="'. \dash\url::kingdom(). '/privacy" target="_blank">'. T_('Privacy Policy') .'</a>';
+
+		\dash\data::termOfService(T_("By clicking Sign Up, you are indicating that you have read the :privacy and agree to the :terms.", ['privacy' => $privacyLink, 'terms' => $termLink]));
 	}
 }
 ?>
