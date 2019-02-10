d6532d57d011c5ad7d19857caba39dc424a58c30
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-10 12:58:21 +0330

    set page title for app

--- addons/content_enter/app/view.php
+++ addons/content_enter/app/view.php
@@ -8,6 +8,11 @@ class view extends \content_enter\home\view
 	public static function config()
 	{
 		parent::config();
+
+		\dash\data::page_special(true);
+		\dash\data::page_title(T_('Enter to :name', ['name' => \dash\data::site_title()]));
+		\dash\data::page_desc(\dash\data::page_title());
+
 	}
 }
 ?>
