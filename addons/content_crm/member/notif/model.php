d7dd6e53735c813894beb15dc7edb7762c825724
Reza <rm.biqarar@gmail.com>
2019-02-18 15:55:33 +0330

    add notif to android

--- addons/content_crm/member/notif/model.php
+++ addons/content_crm/member/notif/model.php
@@ -7,17 +7,23 @@ class model
 	public static function post()
 	{
 
-		$user_id              = \dash\coding::decode(\dash\request::get('id'));
-		$request              = [];
-		// $request['mytext']    = \dash\request::post('notif');
-		$request['myexcerpt'] = \dash\request::post('notif');
-		$request['mytitle']   = \dash\request::post('title');
-		$request['to']        = $user_id;
-		$request['mycat']     = '';
-		$request['iconClass'] = '';
-		$request['icon']      = '';
+		$user_id                     = \dash\coding::decode(\dash\request::get('id'));
+		$request                     = [];
+		$request["notif_title"]      = \dash\request::post('title');
+		$request["notif_small"]      = \dash\request::post('small');
+		$request["notif_big"]        = \dash\request::post('big');
+		$request["notif_sub_text"]   = \dash\request::post('sub_text');
+		$request["notif_group"]      = \dash\request::post('group');
+		$request["notif_large_icon"] = \dash\request::post('large_icon');
+		$request["notif_icon"]       = \dash\request::post('icon');
+		$request["notif_link"]       = \dash\request::post('link');
+		$request["notif_external"]   = \dash\request::post('external') ? true : false;
+		$request['to']               = $user_id;
+		// $request['mycat']         = '';
+		// $request['iconClass']     = '';
+		// $request['icon']          = '';
 
-		$request['from']   = \dash\user::id();
+		$request['from']       = \dash\user::id();
 
 		\dash\log::set('notif_text', $request);
 
--- lib/app/log/caller/notif/notif_text.php
+++ lib/app/log/caller/notif/notif_text.php
@@ -6,20 +6,24 @@ class notif_text
 {
 	public static function site($_args = [])
 	{
-		$text      = isset($_args['data']['mytext']) ? $_args['data']['mytext'] : null;
-		$title     = isset($_args['data']['mytitle']) ? $_args['data']['mytitle'] : T_("Notification");
-		$cat       = isset($_args['data']['mycat']) ? $_args['data']['mycat'] : T_("Notification");
-		$iconClass = isset($_args['data']['iconClass']) ? $_args['data']['iconClass'] : 'fc-green';
-		$icon      = isset($_args['data']['icon']) ? $_args['data']['icon'] : 'bullhorn';
-		$excerpt   = isset($_args['data']['myexcerpt']) ? $_args['data']['myexcerpt'] : null;
 
-		$result              = [];
-		$result['title']     = $title;
-		$result['icon']      = $icon;
-		$result['cat']       = $cat;
-		$result['iconClass'] = $iconClass;
-		$result['excerpt']   = $excerpt;
-		$result['txt']       = $text;
+		$result               = [];
+
+		$result["title"]      = isset($_args['data']['notif_title']) ? $_args['data']['notif_title'] : null;
+		$result["small"]      = isset($_args['data']['notif_small']) ? $_args['data']['notif_small'] : null;
+		$result["big"]        = isset($_args['data']['notif_big']) ? $_args['data']['notif_big'] : null;
+		$result["sub_text"]   = isset($_args['data']['notif_sub_text']) ? $_args['data']['notif_sub_text'] : null;
+		$result["group"]      = isset($_args['data']['notif_group']) ? $_args['data']['notif_group'] : null;
+		$result["sender"]     = isset($_args['data']['notif_sender']) ? $_args['data']['notif_sender'] : null;
+		$result["large_icon"] = isset($_args['data']['notif_large_icon']) ? $_args['data']['notif_large_icon'] : null;
+		$result["icon"]       = isset($_args['data']['notif_icon']) ? $_args['data']['notif_icon'] : null;
+		$result["link"]       = isset($_args['data']['notif_link']) ? $_args['data']['notif_link'] : null;
+		$result["external"]   = isset($_args['data']['notif_external']) ? $_args['data']['notif_external'] : null;
+
+		$result['excerpt']    = $result['small'];
+		$result['cat']        = $result['group'];
+		$result['iconClass']  = 'fc-blue';
+		$result['txt']        = $result['big'];
 
 		return $result;
 
