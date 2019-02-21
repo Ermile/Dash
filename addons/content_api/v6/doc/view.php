9b1cf00fbc5d5a670b365644a7bad32924d7d227
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-21 15:58:21 +0330

    add some detail into api doc

--- addons/content_api/v6/doc/view.php
+++ addons/content_api/v6/doc/view.php
@@ -11,6 +11,8 @@ class view
 
 		\dash\data::page_title( T_(':val API documentation v6', ['val' => \dash\data::site_title()]));
 		\dash\data::page_desc(T_('Last modified'). ' '. \dash\datetime::fit('2019-02-20 18:40', 'human', 'year'));
+		\dash\data::page_pictogram('campfire');
+
 	}
 }
 ?>
