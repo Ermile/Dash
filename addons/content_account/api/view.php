b4f6be533eb3031d664c472dea38695f18163be7
Reza <rm.biqarar@gmail.com>
2019-02-24 09:14:34 +0330

    show api link

--- addons/content_account/api/view.php
+++ addons/content_account/api/view.php
@@ -7,6 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::apikey(\dash\app\user_auth::get_apikey(\dash\user::id(), 'api'));
+		\dash\data::myTitle(T_(':val API documentation', ['val' => \dash\data::site_title()]));
 	}
 }
 ?>
