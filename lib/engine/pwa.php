63d2aa124c210a356c05a11a62f4b47ab29a2520
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-10 08:13:33 +0430

    automatically check icons of manifest

--- lib/engine/pwa.php
+++ lib/engine/pwa.php
@@ -7,10 +7,11 @@ class pwa
 {
 	public static function manifest()
 	{
-		$val =
+		$siteTitle = \dash\option::config('site', 'title');
+		$manifest  =
 		[
-			'name'             => T_(\dash\option::config('site', 'title')). ' | '. T_(\dash\option::config('site', 'slogan')),
-			'short_name'       => T_(\dash\option::config('site', 'title')),
+			'name'             => T_($siteTitle). ' | '. T_(\dash\option::config('site', 'slogan')),
+			'short_name'       => T_($siteTitle),
 			'description'      => T_(\dash\option::config('site', 'desc')),
 			'dir'              => \dash\language::current('direction'),
 			'lang'             => str_replace('_', '-', \dash\language::current('iso')),
@@ -18,27 +19,18 @@ class pwa
 
 			'display'          => 'standalone',
 			// phone top nav color
+			// get color from settings
 			'theme_color'      => '#ffffff',
 			// background of splash
+			// get color from settings
 			'background_color' => '#1a2733',
+
 
 			'scope'            => '/',
 			'start_url'        => \dash\url::kingdom(). '?utm_source=pwa',
 			'orientation'      => 'portrait',
 
-			'icons'            =>
-			[
-				[
-					'type' => 'image/png',
-					'sizes' => '192x192',
-					'src' => \dash\url::static().'/images/favicons/android-chrome-192x192.png',
-				],
-				[
-					'type' => 'image/png',
-					'sizes' => '512x512',
-					'src' => \dash\url::static().'/images/favicons/android-chrome-512x512.png',
-				]
-			],
+
 			// 'related_applications' =>
 			// [
 			// 	[
@@ -50,11 +42,60 @@ class pwa
 			// 		'platform' => 'itunes',
 			// 		'url'      => 'https://itunes.apple.com/app/ermile-jibres/id123456789',
 			// 	]
-			// ]
+			// ],
 		];
 
 
-		\dash\code::jsonBoom($val, true, 'manifest');
+		// set icons if exist
+		$iconsArr = [];
+
+		// check icon192
+		$icon192 = '/images/logo/png/'. $siteTitle. '-logo-192.png';
+		if(file_exists(root. 'public_html/static'. $icon192))
+		{
+			$icon192 = \dash\url::static(). $icon192;
+			$iconsArr[] =
+			[
+				'type' => 'image/png',
+				'sizes' => '192x192',
+				'src' => $icon192,
+			];
+		}
+
+		// check icon512
+		$icon512 = '/images/logo/png/'. $siteTitle. '-logo-512.png';
+		if(file_exists(root. 'public_html/static'. $icon512))
+		{
+			$icon512 = \dash\url::static(). $icon512;
+			$iconsArr[] =
+			[
+				'type' => 'image/png',
+				'sizes' => '512x512',
+				'src' => $icon512,
+			];
+		}
+
+
+		// check default logo
+		$iconDefault = '/images/logo/'. $siteTitle. '.png';
+		if(file_exists(root. 'public_html/static'. $iconDefault))
+		{
+			$iconDefault = \dash\url::static(). $iconDefault;
+			$iconsArr[] =
+			[
+				'type' => 'image/png',
+				'src' => $iconDefault,
+			];
+		}
+
+		if($iconsArr)
+		{
+			$manifest['icons'] = $iconsArr;
+		}
+
+
+
+		\dash\code::jsonBoom($manifest, true, 'manifest');
 	}
 }
 ?>
