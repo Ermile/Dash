c7f24ac87417a2e03fe6b19eee62ebb812275ee4
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-10 09:04:03 +0430

    add some other detail for pwa

--- lib/engine/pwa.php
+++ lib/engine/pwa.php
@@ -126,6 +126,31 @@ evt.waitUntil(
     })
 );
 
+evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+);
+
+if (evt.request.mode !== 'navigate') {
+  // Not a page navigation, bail.
+  return;
+}
+evt.respondWith(
+    fetch(evt.request)
+        .catch(() => {
+          return caches.open(CACHE_NAME)
+              .then((cache) => {
+                return cache.match('offline.html');
+              });
+        })
+);
+
 		";
 
 
