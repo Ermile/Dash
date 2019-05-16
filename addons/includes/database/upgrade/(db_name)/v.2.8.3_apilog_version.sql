1f4dcb410989957fee68d80dd2bc68513af0e667
Reza <rm.biqarar@gmail.com>
2019-05-16 10:39:01 +0430

    add index on apilog table

--- addons/includes/database/upgrade/(db_name)/v.2.8.3_apilog_version.sql
+++ addons/includes/database/upgrade/(db_name)/v.2.8.3_apilog_version.sql
@@ -1,2 +1,21 @@
 -- [database log]
 ALTER TABLE `apilog` ADD `version` varchar(100) NULL DEFAULT NULL;
+ALTER TABLE `apilog` ADD `responselen` int(10) unsigned NULL DEFAULT NULL;
+
+
+
+ALTER TABLE `apilog` ADD INDEX `index_search_version` (`version`);
+ALTER TABLE `apilog` ADD INDEX `index_search_token` (`token`);
+ALTER TABLE `apilog` ADD INDEX `index_search_apikey` (`apikey`);
+ALTER TABLE `apilog` ADD INDEX `index_search_appkey` (`appkey`);
+ALTER TABLE `apilog` ADD INDEX `index_search_zoneid` (`zoneid`);
+
+ALTER TABLE `apilog` ADD INDEX `index_search_method` (`method`);
+ALTER TABLE `apilog` ADD INDEX `index_search_headerlen` (`headerlen`);
+ALTER TABLE `apilog` ADD INDEX `index_search_bodylen` (`bodylen`);
+ALTER TABLE `apilog` ADD INDEX `index_search_pagestatus` (`pagestatus`);
+ALTER TABLE `apilog` ADD INDEX `index_search_resultstatus` (`resultstatus`);
+ALTER TABLE `apilog` ADD INDEX `index_search_responselen` (`responselen`);
+
+
+
