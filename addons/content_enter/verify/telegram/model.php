7b1a2741e038b1696e063f14d1abdb98b6eec68f
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-08-14 21:08:27 +0430

    remove enter form tg enter message

--- addons/content_enter/verify/telegram/model.php
+++ addons/content_enter/verify/telegram/model.php
@@ -52,8 +52,7 @@ class model
 		$text = '';
 		// $text .= T_("Your login code is :code", ['code' => \dash\utility\human::number($code)]);
 		$text .= T_("Your login code is :code", ['code' => '<code>'. $code. '</code>']);
-		$text .= "\n\n". T_("This code can be used to log in to your account. Do not give it to anyone!");
-		$text .= "\n" . T_("If you didn't request this code, ignore this message.");
+		$text .= "\n\n". T_("This code can be used to log in to your account. Do not give it to anyone!"). ' ' . T_("If you didn't request this code, ignore this message.");
 
 		if(\dash\url::isLocal())
 		{
