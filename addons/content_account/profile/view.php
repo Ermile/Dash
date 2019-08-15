c25259a2784ddf1510fca034446a30369a88edd3
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-08-15 19:06:07 +0430

    fix profile page

--- addons/content_account/profile/view.php
+++ addons/content_account/profile/view.php
@@ -7,11 +7,11 @@ class view
 
 	public static function config()
 	{
-		\dash\data::page_title(T_('Edit your profile'));
-		\dash\data::page_desc(T_('You can edit your profile.'));
+		\dash\data::page_title(T_('Personal info'));
+		\dash\data::page_desc(T_('Basic info, like your name and photo, that you use on our services'));
 
-		\dash\data::badge_link(\dash\url::kingdom(). '/a');
-		\dash\data::badge_text(T_('Back to dashbaord'));
+		\dash\data::badge_link(\dash\url::here());
+		\dash\data::badge_text(T_('Back to Account'));
 
 		$id = \dash\user::id();
 
