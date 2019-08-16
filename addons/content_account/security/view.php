7ddd2241ed388f22e20948f367d86d5ba0e381dd
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-08-16 17:21:30 +0430

    add some box to security

--- addons/content_account/security/view.php
+++ addons/content_account/security/view.php
@@ -13,6 +13,8 @@ class view
 		\dash\data::badge_link(\dash\url::here());
 		\dash\data::badge_text(T_('Back to Account'));
 
+		\dash\data::isLtr(\dash\language::current('direction') === 'ltr' ? true : false);
+
 		$id = \dash\user::id();
 
 		if(!$id)
