6717e469f9ef6a05c0151c237345602f737fd0ec
Reza <rm.biqarar@gmail.com>
2019-08-19 14:58:29 +0430

    show apikey and appkey in security

--- addons/content_account/api/view.php
+++ addons/content_account/api/view.php
@@ -6,6 +6,12 @@ class view
 
 	public static function config()
 	{
+		\dash\data::page_title(T_('API key'));
+
+
+		\dash\data::badge_link(\dash\url::here(). '/security');
+		\dash\data::badge_text(T_('Back to Account security'));
+
 		\dash\data::apikey(\dash\app\user_auth::get_apikey(\dash\user::id(), 'api'));
 		\dash\data::myTitle(T_(':val API documentation', ['val' => \dash\data::site_title()]));
 	}
