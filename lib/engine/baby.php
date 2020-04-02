519e02bc73ef26024b5090283a52180fb7ae5b5f
Reza <rm.biqarar@gmail.com>
2020-04-02 14:14:28 +0430

    update baby check

--- lib/engine/baby.php
+++ lib/engine/baby.php
@@ -29,6 +29,14 @@ class baby
 		// check for requests
 		foreach ($_REQUEST as $key => $value)
 		{
+			if(mb_strlen($key) > 20)
+			{
+				self::pacifier(10, 414);
+			}
+			if(mb_strlen($value) > 2000)
+			{
+				self::pacifier(11, 414);
+			}
 			// check key is not using invalid chars
 			self::check($key, true);
 
@@ -36,19 +44,26 @@ class baby
 			{
 				foreach ($value as $key2 => $value2)
 				{
+					if(mb_strlen($key2) > 20)
+					{
+						self::pacifier(12, 414);
+					}
+					if(mb_strlen($value2) > 200)
+					{
+						self::pacifier(13, 414);
+					}
+
 					// check key2 is not using invalid chars
 					self::check($key2, true);
 					if(is_array($value2))
 					{
 						// now we are not allow to give object in array request
-						self::$level = 20;
-						self::pacifier();
+						self::pacifier(20, 415);
 					}
 					else if(is_object($value2))
 					{
 						// now we are not allow to give object in array request
-						self::$level = 11;
-						self::pacifier();
+						self::pacifier(11, 415);
 					}
 					else
 					{
@@ -59,8 +74,7 @@ class baby
 			else if(is_object($value))
 			{
 				// now we are not allow to give object in request
-				self::$level = 10;
-				self::pacifier();
+				self::pacifier(10, 415);
 			}
 			else
 			{
@@ -69,6 +83,7 @@ class baby
 		}
 		// we can add some check on php://input and maybe another one!
 	}
+
 
 
 	private static function dbl_slash()
@@ -81,7 +96,7 @@ class baby
 			// http://dash.local/enter?referer=http://dash.local/cp
 			if(strpos($_SERVER['REQUEST_URI'], '?') === false || strpos($_SERVER['REQUEST_URI'], '?') > strpos($_SERVER['REQUEST_URI'], '//'))
 			{
-				\dash\header::status(421, 'What are you doing!');
+				self::pacifier(18, 421);
 			}
 		}
 	}
@@ -89,18 +104,23 @@ class baby
 	/**
 	 * check duble slass in url
 	 */
-	private static function pacifier()
+	public static function pacifier($_level = null, $_status_code = 418)
 	{
-		$msg = 'Hi Baby'. str_repeat('!', self::$level);
+		if($_level === null)
+		{
+			$_level = self::$level;
+		}
+
+		$msg = 'Hi Baby ('. $_level. ')';
 		if(\dash\request::json_accept() || \dash\request::ajax())
 		{
-			\dash\header::status(418, $msg. ' Are you healthy?');
+			\dash\header::status($_status_code, "Anomalous disturbance has occurred in the transmitted values. We are unable to respond to this request.". ' '. str_repeat('!', $_level));
 		}
 		else
 		{
-			\dash\header::status(418, $msg);
+			\dash\header::status($_status_code, $msg);
 		}
-		self::$level = null;
+
 	}
 
 
@@ -113,11 +133,18 @@ class baby
 	public static function check($_txt, $_block = false)
 	{
 		$result = null;
+		$status_code = 418;
 		// decode url
 		$_txt = urldecode($_txt);
 		// check for problem in hex
 		if(self::hex($_txt))
 		{
+			$result = true;
+		}
+		// check for someone try inject script
+		if(self::script($_txt))
+		{
+			$status_code = 451;
 			$result = true;
 		}
 		// check for problem for containing forbidden chars
@@ -128,7 +155,7 @@ class baby
 		// if needed block
 		if($result === true && $_block)
 		{
-			self::pacifier();
+			self::pacifier(null, $status_code);
 		}
 		// return final result if not blocked!
 		return $result;
@@ -150,6 +177,88 @@ class baby
 		if(preg_match("#0x#", $_txt))
 		{
 			self::$level = 2;
+			return true;
+		}
+		// if cant find something return false
+		return false;
+	}
+
+
+	/**
+	 * check some problem on hexas input or someother things
+	 * @param  [type] $_txt [description]
+	 * @return [type]       [description]
+	 */
+	public static function script($_txt)
+	{
+		if(preg_match("/<script>/i", $_txt))
+		{
+			self::$level = 1;
+			return true;
+		}
+		if(preg_match("/<\/script>/i", $_txt))
+		{
+			self::$level = 2;
+			return true;
+		}
+		if(preg_match("/<\s+script/i", $_txt))
+		{
+			self::$level = 3;
+			return true;
+		}
+		if(preg_match("/<(.*)script/i", $_txt))
+		{
+			self::$level = 4;
+			return true;
+		}
+		if(preg_match("/alert(.*)\(/i", $_txt))
+		{
+			self::$level = 5;
+			return true;
+		}
+		if(preg_match("/<(.*)>/", $_txt))
+		{
+			self::$level = 3;
+			return true;
+		}
+		if(preg_match("/<(.*)\?/", $_txt))
+		{
+			self::$level = 2;
+			return true;
+		}
+		if(preg_match("/</", $_txt))
+		{
+			self::$level = 1;
+			return true;
+		}
+		if(preg_match("/eval(.*)\(/i", $_txt))
+		{
+			self::$level = 0;
+			return true;
+		}
+		if(preg_match("/sleep(.*)\((.*)\)/i", $_txt))
+		{
+			self::$level = 2;
+			return true;
+		}
+		if(preg_match("/extractvalue(.*)\(/i", $_txt))
+		{
+			self::$level = 3;
+			return true;
+		}
+		if(preg_match("/xmltype(.*)\(/i", $_txt))
+		{
+			self::$level = 4;
+			return true;
+		}
+		if(preg_match("/\/([^\/]*)(and|or)(\s|\()+(.*)=(.*)/i", $_txt))
+		{
+			self::$level = 6;
+			return true;
+		}
+		if(preg_match("/\/([^\/]*)union(.*)(\(|\=|\))=(.*)/i", $_txt))
+		{
+			self::$level = 5;
 			return true;
 		}
 		// if cant find something return false
